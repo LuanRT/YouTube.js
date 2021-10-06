9956d286df14e42876716f28c52cccabea5c6873
LuanRT <luan.lrt4@gmail.com>
2021-10-06 02:04:07 -0300

    test: update examples file

--- examples/index.js
+++ examples/index.js
@@ -4,14 +4,14 @@ const COOKIE = 'YT_COOKIE_HERE';
 
 async function start() {
   const youtube = await new Innertube(COOKIE);
-  
+
   // Searching, getting details about videos & making interactions:
   const search = await youtube.search('Looking for life on Mars - documentary');
   console.info('Search results:', search);
-  
+
   const video = await youtube.getDetails(search.videos[0].id);
   console.info('Video details:', video);
-  
+
   if (youtube.logged_in) {
     const myNotifications = await youtube.getNotifications();
     console.info('My notifications:', myNotifications);
@@ -35,18 +35,18 @@ async function start() {
     if (myComment.success) {
       console.info('Comment successfully posted!')
     }
-    
+
     const subscribe = await video.subscribe();
     if (subscribe.success) {
       console.info('Just subscribed to', video.metadata.channel_name + '!');
     }
-    
+
     const unsubscribe = await video.unsubscribe();
     if (unsubscribe.success) {
-      console.info('Just unsubscribed from', video.metadata.channel_name +' :(');
+      console.info('Just unsubscribed from', video.metadata.channel_name + ' :(');
     }
   }
-  
+
   // Downloading videos:
   const stream = youtube.download(search.videos[0].id, {
     format: 'mp4', // Optional, ignored when type is set to audio and defaults to mp4, and I recommend to leave it as it is
@@ -64,15 +64,7 @@ async function start() {
     console.info('[DOWNLOADER]', `Downloading ${info.video_details.title} by ${info.video_details.metadata.channel_name}`);
   });
 
-  stream.on('progress', (info) => {
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
-    process.stdout.write(`[DOWNLOADER] Downloaded ${info.percentage}% (${info.downloaded_size}MB) of ${info.size}MB`);
-  });
-
   stream.on('end', () => {
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
     console.info('[DOWNLOADER]', 'Done!');
   });
 
