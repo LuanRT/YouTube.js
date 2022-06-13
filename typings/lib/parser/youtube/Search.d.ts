6aa30648fef6c9dc797b6ddc8597a66a39022b82
LuanRT <luan.lrt4@gmail.com>
2022-06-13 16:23:49 -0300

    chore: update type definitions

--- typings/lib/parser/youtube/Search.d.ts
+++ typings/lib/parser/youtube/Search.d.ts
@@ -11,25 +11,42 @@ declare class Search {
     constructor(response: object, actions: import('../../core/Actions'), args?: {
         is_continuation?: boolean;
     });
-    results: any;
+    /** @type {object[]} */
+    results: object[];
     refinements: any;
     estimated_results: any;
-    sections: any;
-    refinement_cards: {
-        header: any;
-        cards: any;
+    /** @type {{ sections: { title: string; items: object[]; }[] }} */
+    sections: {
+        sections: {
+            title: string;
+            items: object[];
+        }[];
     };
+    refinement_cards: {
+        /** @type {import('../contents/classes/RichListHeader')} */
+        header: import('../contents/classes/RichListHeader');
+        /** @type {import('../contents/classes/SearchRefinementCard')} */
+        cards: import('../contents/classes/SearchRefinementCard');
+    };
+    /**
+     * Retrieves next batch of results.
+     * @returns {Promise.<Search>}
+     */
     getContinuation(): Promise<Search>;
     /**
      * Applies given refinement card and returns a new {@link Search} object.
-     * @param {SearchRefinementCard || string} refinement card object or query
-     * @returns {Search}
+     * @param {SearchRefinementCard | string} card - refinement card object or query
+     * @returns {Promise.<Search>}
      */
-    selectRefinementCard(card: any): Search;
+    selectRefinementCard(card: SearchRefinementCard | string): Promise<Search>;
+    /** @type {boolean} */
     get has_continuation(): boolean;
-    get refinement_card_queries(): any;
-    get videos(): any;
-    get playlists(): any;
+    /** @type {string[]} */
+    get refinement_card_queries(): string[];
+    /** @type {import('../contents/classes/Video')[]} */
+    get videos(): import("../contents/classes/Video")[];
+    /** @type {import('../contents/classes/Playlist')[]} */
+    get playlists(): import("../contents/classes/Playlist")[];
     get page(): any;
     #private;
 }
