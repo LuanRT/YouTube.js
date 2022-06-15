9fb7cdecfe3c2cf801e67c0882bb70601f51dacc
Daniel Wykerd <danielwykerd@gmail.com>
2022-06-15 21:24:30 +0200

    refactor: remove Simplify completely

--- typings/lib/parser/simplify.d.ts
+++
@@ -1,9 +0,0 @@
-export = Simplify;
-declare class Simplify {
-    static matching(match: any): Simplify;
-    static matchingAny(match: any): Simplify;
-    constructor(schema: any);
-    match(data: any): any[];
-    runOn(data: any): any;
-    #private;
-}
