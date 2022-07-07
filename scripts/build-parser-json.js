670b9186421ce3f5f383a5c4dc38bc7ecca34127
LuanRT <luan.lrt4@gmail.com>
2022-07-07 03:42:21 -0300

    style: format code

--- scripts/build-parser-json.js
+++ scripts/build-parser-json.js
@@ -5,28 +5,33 @@ const fs = require('fs');
 
 const json = [ '{' ];
 
-glob.sync('../lib/parser/contents/classes/**/*.js', { cwd: __dirname }).forEach((
-  file
-) => {
-  // Trim path
-  file = file.replace('../lib/parser/contents/classes/', '').replace('.js', '');
-  json.push(`'${file}': () => require('./classes/${file}'),`);
-});
+glob.sync('../lib/parser/contents/classes/**/*.js', { cwd: __dirname })
+  .forEach((file) => {
+    // Trim path
+    file = file.replace('../lib/parser/contents/classes/', '').replace('.js', '');
+    json.push(`'${file}': () => require('./classes/${file}'),`);
+  });
 
 json.push('}');
+
 fs.writeFileSync(
   './lib/parser/contents/map.js',
-  `/* eslint-disable */
-// This file was automatically generated, do not edit
+  `// This file was auto generated, do not edit.
 // See ./scripts/build-parser-json.js
+
+/* eslint-disable */
+
 const map = ${json.join('')};
+
 module.exports = function req(name) {
   const func = map[name];
+ 
   if (!func) {
     const error = new Error('Module not found: ' + name);
     error.code = 'MODULE_NOT_FOUND';
     throw error;
   }
+  
   return func();
 }`
 );
