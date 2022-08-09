dc79b19d56f0601229c8a5f168532fbcf518358a
patrickkfkan <55383971+patrickkfkan@users.noreply.github.com>
2022-08-10 02:37:19 +0800

    refactor: migrate `MusicCarouselShelfBasicHeader` to TypeScript and add `more_content` prop (#132)
    * refactor: migrate `MusicCarouselShelfBasicHeader` to TS
    
    * feat: `MusicCarouselShelfBasicHeader.more_content`

---
+++ src/parser/classes/MusicCarouselShelfBasicHeader.ts
@@ -0,0 +1,35 @@
+import Text from './misc/Text';
+import { YTNode } from '../helpers';
+import MusicThumbnail from './MusicThumbnail';
+import Parser from '..';
+import Button from './Button';
+
+class MusicCarouselShelfBasicHeader extends YTNode {
+  static type = 'MusicCarouselShelfBasicHeader';
+
+  strapline?: Text;
+  title: Text;
+  thumbnail?: MusicThumbnail | null;
+  more_content?: Button | null;
+
+  constructor(data: any) {
+    super();
+
+    if (data.strapline) {
+      this.strapline = new Text(data.strapline);
+    }
+
+    this.title = new Text(data.title);
+    // This.label = data.accessibilityData.accessibilityData.label;
+    // ^^ redundant?
+    if (data.thumbnail) {
+      this.thumbnail = Parser.parseItem<MusicThumbnail>(data.thumbnail, MusicThumbnail);
+    }
+
+    if (data.moreContentButton) {
+      this.more_content = Parser.parseItem<Button>(data.moreContentButton, Button);
+    }
+  }
+}
+
+export default MusicCarouselShelfBasicHeader;
