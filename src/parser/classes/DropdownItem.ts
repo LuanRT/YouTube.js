8ff5c279e5ac43cc15817180c091535a9311a34e
patrickkfkan <patrickkfkan@gmail.com>
2022-08-14 02:55:38 +0800

    feat: add `endpoint` to `DropdownItem`

--- src/parser/classes/DropdownItem.ts
+++ src/parser/classes/DropdownItem.ts
@@ -1,20 +1,29 @@
 import { YTNode } from '../helpers';
 import Text from './misc/Text';
+import NavigationEndpoint from './NavigationEndpoint';
 
 class DropdownItem extends YTNode {
   static type = 'DropdownItem';
 
   label: string;
   selected: boolean;
-  value: number;
+  value?: number;
   iconType?: string;
+  endpoint?: NavigationEndpoint;
 
   constructor(data: any) {
     super();
 
     this.label = new Text(data.label).toString();
     this.selected = !!data.isSelected;
-    this.value = data.int32Value;
+
+    if (data.int32Value) {
+      this.value = data.int32Value;
+    }
+
+    if (data.onSelectCommand?.browseEndpoint) {
+      this.endpoint = new NavigationEndpoint(data.onSelectCommand);
+    }
 
     if (data.icon?.iconType) {
       this.iconType = data.icon?.iconType;
