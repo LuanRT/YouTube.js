8b547ffb9ec5e69d75210f53ffe46d67a7321203
patrickkfkan <patrickkfkan@gmail.com>
2022-08-19 17:14:56 +0800

    fix: `PlaylistPanel` err when no continuation

--- src/parser/classes/PlaylistPanel.ts
+++ src/parser/classes/PlaylistPanel.ts
@@ -25,7 +25,7 @@ class PlaylistPanel extends YTNode {
     this.contents = Parser.parseArray<PlaylistPanelVideo | AutomixPreviewVideo>(data.contents, [ PlaylistPanelVideo, AutomixPreviewVideo ]);
     this.playlist_id = data.playlistId;
     this.is_infinite = data.isInfinite;
-    this.continuation = data.continuations[0]?.nextRadioContinuationData?.continuation || data.continuations[0]?.nextContinuationData?.continuation;
+    this.continuation = data.continuations?.[0]?.nextRadioContinuationData?.continuation || data.continuations?.[0]?.nextContinuationData?.continuation;
     this.is_editable = data.isEditable;
     this.preview_description = data.previewDescription;
     this.num_items_to_show = data.numItemsToShow;
