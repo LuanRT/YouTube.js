c99364942c928d89643e2814441a3bd8ba4c9ed3
Patrick Kan <55383971+patrickkfkan@users.noreply.github.com>
2022-08-30 16:10:51 +0800

    fix: `DidYouMean` endpoints and add `text` prop (#158)
    * feat: add `text` to `DidYouMean`
    
    * fix: parse correct endpoint in `DidYouMean`

--- src/parser/classes/DidYouMean.ts
+++ src/parser/classes/DidYouMean.ts
@@ -5,13 +5,15 @@ import { YTNode } from '../helpers';
 class DidYouMean extends YTNode {
   static type = 'DidYouMean';
 
+  text: string;
   corrected_query: Text;
   endpoint: NavigationEndpoint;
 
   constructor(data: any) {
     super();
+    this.text = new Text(data.didYouMean).toString();
     this.corrected_query = new Text(data.correctedQuery);
-    this.endpoint = new NavigationEndpoint(data.navigationEndpoint);
+    this.endpoint = new NavigationEndpoint(data.navigationEndpoint || data.correctedQueryEndpoint);
   }
 }
 
