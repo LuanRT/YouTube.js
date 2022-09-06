e3d483ed7567dac7ae66551f88a982059c9772a4
LuanRT <luan.lrt4@gmail.com>
2022-09-06 03:37:14 -0300

    chore(docs): update examples

--- src/core/Studio.ts
+++ src/core/Studio.ts
@@ -57,8 +57,8 @@ class Studio {
    * Uploads a video to YouTube.
    * @example
    * ```ts
-   * const buffer = fs.readFileSync('./my_awesome_video.mp4');
-   * const response = await yt.studio.upload(buffer, { title: 'Wow!' });
+   * const file = fs.readFileSync('./my_awesome_video.mp4');
+   * const response = await yt.studio.upload(file.buffer, { title: 'Wow!' });
    * ```
    */
   async upload(file: BodyInit, metadata: VideoMetadata = {}): Promise<AxioslikeResponse> {
