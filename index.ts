4ef0172497cc631447f300c2c0b37292e45d9402
Salvador Ligayao <futuremr.ligayao03@gmail.com>
2022-09-15 08:01:49 +0800

    added missing import in index.ts

--- index.ts
+++ index.ts
@@ -4,12 +4,14 @@ import { getRuntime } from './src/utils/Utils';
 if (getRuntime() === 'node') {
   // eslint-disable-next-line
   const undici = require('undici');
+  const { ReadableStream } = require('node:stream/web');
   Reflect.set(globalThis, 'fetch', undici.fetch);
   Reflect.set(globalThis, 'Headers', undici.Headers);
   Reflect.set(globalThis, 'Request', undici.Request);
   Reflect.set(globalThis, 'Response', undici.Response);
   Reflect.set(globalThis, 'FormData', undici.FormData);
   Reflect.set(globalThis, 'File', undici.File);
+  Reflect.set(globalThis, 'ReadableStream', ReadableStream);
 }
 
 import Innertube from './src/Innertube';
