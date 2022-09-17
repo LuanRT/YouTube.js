10692c5400e635152bae5c2fc0813c739a92d1bd
Salvador Ligayao <futuremr.ligayao03@gmail.com>
2022-09-17 11:40:13 +0800

    removed extra require

--- index.ts
+++ index.ts
@@ -4,18 +4,17 @@ import { getRuntime } from './src/utils/Utils';
 if (getRuntime() === 'node') {
   // eslint-disable-next-line
   const undici = require('undici');
-  const { ReadableStream } = require('node:stream/web');
   Reflect.set(globalThis, 'fetch', undici.fetch);
   Reflect.set(globalThis, 'Headers', undici.Headers);
   Reflect.set(globalThis, 'Request', undici.Request);
   Reflect.set(globalThis, 'Response', undici.Response);
   Reflect.set(globalThis, 'FormData', undici.FormData);
   Reflect.set(globalThis, 'File', undici.File);
-try {
-  // eslint-disable-next-line
-  const { ReadableStream } = require('node:stream/web');
-  Reflect.set(globalThis, 'ReadableStream', ReadableStream);
-} catch { /* do nothing */ }
+  try {
+    // eslint-disable-next-line
+    const { ReadableStream } = require('node:stream/web');
+    Reflect.set(globalThis, 'ReadableStream', ReadableStream);
+  } catch { /* do nothing */ }
 }
 
 import Innertube from './src/Innertube';
