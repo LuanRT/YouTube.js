088f909515c66952c8ff43e13d71b8f15479780e
LuanRT <luan.lrt4@gmail.com>
2022-10-13 16:52:19 -0300

    chore: update proto

--- src/proto/youtube.ts
+++ src/proto/youtube.ts
@@ -15,19 +15,6 @@ import { reflectionMergePartial } from "@protobuf-ts/runtime";
 import { MESSAGE_TYPE } from "@protobuf-ts/runtime";
 import { MessageType } from "@protobuf-ts/runtime";
 /**
- * @generated from protobuf message youtube.VisitorData
- */
-export interface VisitorData {
-    /**
-     * @generated from protobuf field: string id = 1;
-     */
-    id: string;
-    /**
-     * @generated from protobuf field: int32 timestamp = 5;
-     */
-    timestamp: number;
-}
-/**
  * @generated from protobuf message youtube.ChannelAnalytics
  */
 export interface ChannelAnalytics {
@@ -644,60 +631,6 @@ export interface SearchFilter_Filters {
      */
     duration?: number;
 }
-// @generated message type with reflection information, may provide speed optimized methods
-class VisitorData$Type extends MessageType<VisitorData> {
-    constructor() {
-        super("youtube.VisitorData", [
-            { no: 1, name: "id", kind: "scalar", T: 9 /*ScalarType.STRING*/ },
-            { no: 5, name: "timestamp", kind: "scalar", T: 5 /*ScalarType.INT32*/ }
-        ]);
-    }
-    create(value?: PartialMessage<VisitorData>): VisitorData {
-        const message = { id: "", timestamp: 0 };
-        globalThis.Object.defineProperty(message, MESSAGE_TYPE, { enumerable: false, value: this });
-        if (value !== undefined)
-            reflectionMergePartial<VisitorData>(this, message, value);
-        return message;
-    }
-    internalBinaryRead(reader: IBinaryReader, length: number, options: BinaryReadOptions, target?: VisitorData): VisitorData {
-        let message = target ?? this.create(), end = reader.pos + length;
-        while (reader.pos < end) {
-            let [fieldNo, wireType] = reader.tag();
-            switch (fieldNo) {
-                case /* string id */ 1:
-                    message.id = reader.string();
-                    break;
-                case /* int32 timestamp */ 5:
-                    message.timestamp = reader.int32();
-                    break;
-                default:
-                    let u = options.readUnknownField;
-                    if (u === "throw")
-                        throw new globalThis.Error(`Unknown field ${fieldNo} (wire type ${wireType}) for ${this.typeName}`);
-                    let d = reader.skip(wireType);
-                    if (u !== false)
-                        (u === true ? UnknownFieldHandler.onRead : u)(this.typeName, message, fieldNo, wireType, d);
-            }
-        }
-        return message;
-    }
-    internalBinaryWrite(message: VisitorData, writer: IBinaryWriter, options: BinaryWriteOptions): IBinaryWriter {
-        /* string id = 1; */
-        if (message.id !== "")
-            writer.tag(1, WireType.LengthDelimited).string(message.id);
-        /* int32 timestamp = 5; */
-        if (message.timestamp !== 0)
-            writer.tag(5, WireType.Varint).int32(message.timestamp);
-        let u = options.writeUnknownFields;
-        if (u !== false)
-            (u == true ? UnknownFieldHandler.onWrite : u)(this.typeName, message, writer);
-        return writer;
-    }
-}
-/**
- * @generated MessageType for protobuf message youtube.VisitorData
- */
-export const VisitorData = new VisitorData$Type();
 // @generated message type with reflection information, may provide speed optimized methods
 class ChannelAnalytics$Type extends MessageType<ChannelAnalytics> {
     constructor() {
