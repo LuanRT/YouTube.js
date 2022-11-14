43061970c66b8c767c3a7ac456ba22b67e88f0c7
LuanRT <luan.lrt4@gmail.com>
2022-11-14 15:30:40 -0300

    fix: export `Player` & `Session` classes

--- browser.ts
+++ browser.ts
@@ -6,4 +6,6 @@ export * from './src/utils';
 export { YTNodes } from './src/parser/map';
 export { default as Parser } from './src/parser';
 export { default as Innertube } from './src/Innertube';
+export { default as Session } from './src/core/Session';
+export { default as Player } from './src/core/Player';
 export default Innertube;
