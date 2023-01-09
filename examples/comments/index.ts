5703d5ec9974a5341fd5bd57cddc898fe173777d
LuanRT <luan.lrt4@gmail.com>
2023-01-09 07:27:34 -0300

    docs: update API ref

--- examples/comments/index.ts
+++ examples/comments/index.ts
@@ -3,37 +3,43 @@ import { Innertube, UniversalCache } from 'youtubei.js';
 (async () => {
   const yt = await Innertube.create({ cache: new UniversalCache() });
 
-  const comments = await yt.getComments('a-rqu-hjobc');
- 
-  console.info(`This video has ${comments.header?.comments_count.toString() || 'N/A'} comments.\n`);
+  const comment_section = await yt.getComments('a-rqu-hjobc');
 
-  for (const thread of comments.contents) {
+  console.info(`This video has ${comment_section.header?.comments_count.toString() || 'N/A'} comments.\n`);
+
+  for (const thread of comment_section.contents) {
     const comment = thread.comment;
-    
+
     if (comment) {
       console.info(
+        `${comment.is_pinned ? '[Pinned]' : ''}`,
+        `${comment.is_member ? `${comment.sponsor_comment_badge?.tooltip}` : ''}`,
         `${comment.author.name} • ${comment.published}\n`,
         `${comment.content.toString()}`, '\n',
-        `Likes: ${comment.vote_count.short_text}`, '\n'
+        `Likes: ${comment.vote_count}`, '\n'
       );
-     
-      if (comment.reply_count > 0) {
+
+      if (thread.has_replies) {
         console.info('Replies:', '\n');
 
-        const comment_thread = await thread.getReplies();
-       
-        if (comment_thread.replies) {
-          for (const reply of comment_thread.replies) {
+        let comment_thread = await thread.getReplies();
+
+        while (true) {
+          for (const reply of comment_thread?.replies || []) {
             console.info(
               `> ${reply.author.name} • ${reply.published}\n`,
               `${reply.content.toString()}`, '\n',
-              `Likes: ${reply.vote_count.short_text}`, '\n'
+              `Likes: ${reply.vote_count}`, '\n'
             );
           }
+
+          try {
+            comment_thread = await comment_thread.getContinuation();
+          } catch { break; };
         }
       }
     }
-    
+
     console.log('\n');
   }
 })();
