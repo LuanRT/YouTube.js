644f8bc042cb8829fc051a846e0d431b9334f373
Daniel Wykerd <danielwykerd@gmail.com>
2023-01-31 18:32:47 +0200

    feat(parser): Text#toHTML
    Added support to render Text nodes as HTML for use in web applications.

--- src/parser/classes/misc/TextRun.ts
+++ src/parser/classes/misc/TextRun.ts
@@ -1,6 +1,7 @@
 import NavigationEndpoint from '../NavigationEndpoint';
+import { escape, Run } from './Text';
 
-class TextRun {
+class TextRun implements Run {
   text: string;
   endpoint: NavigationEndpoint | undefined;
   bold: boolean;
@@ -13,6 +14,25 @@ class TextRun {
     this.italics = Boolean(data.italics);
     this.strikethrough = Boolean(data.strikethrough);
     this.endpoint = data.navigationEndpoint ? new NavigationEndpoint(data.navigationEndpoint) : undefined;
+  }
+
+  toString() {
+    return this.text;
+  }
+
+  toHTML(): string {
+    const tags: string[] = [];
+    if (this.bold) tags.push('b');
+    if (this.italics) tags.push('i');
+    if (this.strikethrough) tags.push('s');
+    const escaped_text = escape(this.text);
+    const styled_text = tags.map((tag) => `<${tag}>`).join('') + escaped_text + tags.map((tag) => `</${tag}>`).join('');
+    const wrapped_text = `<span style="white-space: pre-wrap;">${styled_text}</span>`;
+    if (this.endpoint) {
+      const url = this.endpoint.toURL();
+      if (url) return `<a href="${url}">${wrapped_text}</a>`;
+    }
+    return wrapped_text;
   }
 }
 
