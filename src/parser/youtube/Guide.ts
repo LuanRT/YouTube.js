a8562635ff508692de2dc58c82cfc67244419a39
patrickkfkan <patrickkfkan@gmail.com>
2023-03-04 02:21:32 +0800

    fix(Guide): include subscription section

--- src/parser/youtube/Guide.ts
+++ src/parser/youtube/Guide.ts
@@ -2,14 +2,15 @@ import type { IGuideResponse } from '../types/ParsedResponse.js';
 import { IRawResponse, Parser } from '../index.js';
 import { ObservedArray } from '../helpers.js';
 import GuideSection from '../classes/GuideSection.js';
+import { GuideSubscriptionsSection } from '../map.js';
 
 export default class Guide {
 
   #page: IGuideResponse;
-  contents: ObservedArray<GuideSection>;
+  contents: ObservedArray<GuideSection | GuideSubscriptionsSection>;
 
   constructor(data: IRawResponse) {
     this.#page = Parser.parseResponse<IGuideResponse>(data);
-    this.contents = this.#page.items_memo.getType(GuideSection);
+    this.contents = this.#page.items.array().as(GuideSection, GuideSubscriptionsSection);
   }
 }
