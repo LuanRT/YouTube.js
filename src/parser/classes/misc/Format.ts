98e624994fabf945ac542f4b6b796c5c66b15ab0
absidue <48293849+absidue@users.noreply.github.com>
2023-03-05 23:01:51 +0100

    feat: Add support for descriptive audio tracks

--- src/parser/classes/misc/Format.ts
+++ src/parser/classes/misc/Format.ts
@@ -41,6 +41,7 @@ class Format {
   has_video: boolean;
   language?: string | null;
   is_dubbed?: boolean;
+  is_descriptive?: boolean;
   is_original?: boolean;
 
   constructor(data: any) {
@@ -83,6 +84,7 @@ class Format {
 
       this.language = url_components.get('xtags')?.split(':').find((x: string) => x.startsWith('lang='))?.split('=').at(1) || null;
       this.is_dubbed = url_components.get('xtags')?.split(':').find((x: string) => x.startsWith('acont='))?.split('=').at(1) === 'dubbed';
+      this.is_descriptive = url_components.get('xtags')?.split(':').find((x: string) => x.startsWith('acont='))?.split('=').at(1) === 'descriptive';
       this.is_original = url_components.get('xtags')?.split(':').find((x: string) => x.startsWith('acont='))?.split('=').at(1) === 'original' || !this.is_dubbed;
 
       if (data.audioTrack) {
