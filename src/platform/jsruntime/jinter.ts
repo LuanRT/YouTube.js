3c94c9da4ba2ad582e520396886d9023ff3b22f7
LuanRT <luan.lrt4@gmail.com>
2023-04-08 23:53:59 -0300

    deps: bump Jinter to v1.0.0

--- src/platform/jsruntime/jinter.ts
+++ src/platform/jsruntime/jinter.ts
@@ -2,7 +2,7 @@ import Jinter from 'jintr';
 import { VMPrimative } from '../../types/PlatformShim.js';
 
 export default function evaluate(code: string, env: Record<string, VMPrimative>) {
-  const runtime = new Jinter.default(code);
+  const runtime = new Jinter(code);
   for (const [ key, value ] of Object.entries(env)) {
     runtime.scope.set(key, value);
   }
