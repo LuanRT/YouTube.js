3b6ccfa3d8c3c8f174e022bf0f7e2d260cc84e50
LuanRT <luan.lrt4@gmail.com>
2023-04-09 02:18:53 -0300

    chore: fix cjs build

--- src/platform/jsruntime/jinter.ts
+++ src/platform/jsruntime/jinter.ts
@@ -1,4 +1,4 @@
-import Jinter from 'jintr';
+import { Jinter } from 'jintr';
 import { VMPrimative } from '../../types/PlatformShim.js';
 
 export default function evaluate(code: string, env: Record<string, VMPrimative>) {
