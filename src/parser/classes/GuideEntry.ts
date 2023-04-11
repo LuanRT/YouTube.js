8564509f918710b5b5f87cb276c47867606254a0
LuanRT <luan.lrt4@gmail.com>
2023-04-11 05:43:59 -0300

    refactor: fix inconsistencies in the guide nodes

--- src/parser/classes/GuideEntry.ts
+++ src/parser/classes/GuideEntry.ts
@@ -1,9 +1,11 @@
-import Text from './misc/Text.js';
 import NavigationEndpoint from './NavigationEndpoint.js';
-import { YTNode } from '../helpers.js';
+import Text from './misc/Text.js';
 import Thumbnail from './misc/Thumbnail.js';
 
-class GuideEntry extends YTNode {
+import { YTNode } from '../helpers.js';
+import type { RawNode } from '../index.js';
+
+export default class GuideEntry extends YTNode {
   static type = 'GuideEntry';
 
   title: Text;
@@ -13,21 +15,24 @@ class GuideEntry extends YTNode {
   badges?: any;
   is_primary: boolean;
 
-  constructor(data: any) {
+  constructor(data: RawNode) {
     super();
     this.title = new Text(data.formattedTitle);
     this.endpoint = new NavigationEndpoint(data.navigationEndpoint || data.serviceEndpoint);
-    if (data.icon?.iconType) {
+
+    if (Reflect.has(data, 'icon') && Reflect.has(data.icon, 'iconType')) {
       this.icon_type = data.icon.iconType;
     }
-    if (data.thumbnail) {
+
+    if (Reflect.has(data, 'thumbnail')) {
       this.thumbnails = Thumbnail.fromResponse(data.thumbnail);
     }
-    if (data.badges) {
+
+    // (LuanRT) XXX: Check this property's data and parse it.
+    if (Reflect.has(data, 'badges')) {
       this.badges = data.badges;
     }
+
     this.is_primary = !!data.isPrimary;
   }
 }
-
-export default GuideEntry;
