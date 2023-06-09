793dd1babd3176a69ab698b40b22d5449c7469d2
Emma <MarmadileManteater@proton.me>
2023-06-09 17:31:17 -0400

    Replace `String` with `Text` in `ExpandableVideoDescriptionBody`

--- src/parser/classes/ExpandableVideoDescriptionBody.ts
+++ src/parser/classes/ExpandableVideoDescriptionBody.ts
@@ -1,19 +1,20 @@
 import { YTNode } from '../helpers.js';
 import { type RawNode } from '../index.js';
+import { Text } from '../misc.js';
 
 export default class ExpandableVideoDescriptionBody extends YTNode {
   static type = 'ExpandableVideoDescriptionBody';
 
-  show_more_text: String;
-  show_less_text: String;
+  show_more_text: Text;
+  show_less_text: Text;
   attributed_description_body_text: {
     content: String
   };
 
   constructor(data: RawNode) {
     super();
-    this.show_more_text = data.showMoreText.simpleText;
-    this.show_less_text = data.showLessText.simpleText;
+    this.show_more_text = new Text(data.showMoreText);
+    this.show_less_text = new Text(data.showLessText);
     this.attributed_description_body_text = {
       content: data.attributedDescriptionBodyText.content
     };
