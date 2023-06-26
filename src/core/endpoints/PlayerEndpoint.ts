e41d4d717c7d58bc383b91662a53355f4ef5601a
absidue <48293849+absidue@users.noreply.github.com>
2023-06-26 22:18:31 +0200

    fix(PlayerEndpoint): Use new throttling bypass player params

--- src/core/endpoints/PlayerEndpoint.ts
+++ src/core/endpoints/PlayerEndpoint.ts
@@ -38,7 +38,7 @@ export function build(opts: PlayerEndpointOptions): IPlayerRequest {
       client: opts.client,
       playlistId: opts.playlist_id,
       // Workaround streaming URLs returning 403 when using Android clients and throttling in web clients.
-      params: 'CgIQBg=='
+      params: '2AMBCgIQBg'
     }
   };
 }
