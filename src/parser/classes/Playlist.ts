69b224d7575638ee174fdc0166deecca55bbc432
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2023-07-13 09:25:55 -0700

    Fix(Playlist): Parse `PlaylistCustomThumbnail`

--- src/parser/classes/Playlist.ts
+++ src/parser/classes/Playlist.ts
@@ -1,6 +1,7 @@
 import { YTNode, type ObservedArray } from '../helpers.js';
 import Parser, { type RawNode } from '../index.js';
 import NavigationEndpoint from './NavigationEndpoint.js';
+import PlaylistCustomThumbnail from './PlaylistCustomThumbnail.js';
 import PlaylistVideoThumbnail from './PlaylistVideoThumbnail.js';
 import Author from './misc/Author.js';
 import Text from './misc/Text.js';
@@ -13,7 +14,7 @@ export default class Playlist extends YTNode {
   title: Text;
   author: Text | Author;
   thumbnails: Thumbnail[];
-  thumbnail_renderer?: PlaylistVideoThumbnail;
+  thumbnail_renderer?: PlaylistVideoThumbnail | PlaylistCustomThumbnail;
   video_count: Text;
   video_count_short: Text;
   first_videos: ObservedArray<YTNode>;
@@ -44,7 +45,7 @@ export default class Playlist extends YTNode {
     this.thumbnail_overlays = Parser.parseArray(data.thumbnailOverlays);
 
     if (Reflect.has(data, 'thumbnailRenderer')) {
-      this.thumbnail_renderer = Parser.parseItem(data.thumbnailRenderer, PlaylistVideoThumbnail) || undefined;
+      this.thumbnail_renderer = Parser.parseItem(data.thumbnailRenderer, [ PlaylistVideoThumbnail, PlaylistCustomThumbnail ]) || undefined;
     }
 
     if (Reflect.has(data, 'viewPlaylistText')) {
