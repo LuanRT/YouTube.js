b50d1ef67d81276864818de10c61b5a7980cbc1a
LuanRT <luan.lrt4@gmail.com>
2023-07-16 17:00:36 -0300

    fix(StructuredDescriptionContent): `items` can also be a `HorizontalCardList`

--- src/parser/classes/StructuredDescriptionContent.ts
+++ src/parser/classes/StructuredDescriptionContent.ts
@@ -1,6 +1,7 @@
 import { YTNode, type ObservedArray } from '../helpers.js';
 import Parser, { type RawNode } from '../index.js';
 import ExpandableVideoDescriptionBody from './ExpandableVideoDescriptionBody.js';
+import HorizontalCardList from './HorizontalCardList.js';
 import VideoDescriptionHeader from './VideoDescriptionHeader.js';
 import VideoDescriptionInfocardsSection from './VideoDescriptionInfocardsSection.js';
 import VideoDescriptionMusicSection from './VideoDescriptionMusicSection.js';
@@ -8,10 +9,10 @@ import VideoDescriptionMusicSection from './VideoDescriptionMusicSection.js';
 export default class StructuredDescriptionContent extends YTNode {
   static type = 'StructuredDescriptionContent';
 
-  items: ObservedArray<VideoDescriptionHeader | ExpandableVideoDescriptionBody | VideoDescriptionMusicSection | VideoDescriptionInfocardsSection>;
+  items: ObservedArray<VideoDescriptionHeader | ExpandableVideoDescriptionBody | VideoDescriptionMusicSection | VideoDescriptionInfocardsSection | HorizontalCardList>;
 
   constructor(data: RawNode) {
     super();
-    this.items = Parser.parseArray(data.items, [ VideoDescriptionHeader, ExpandableVideoDescriptionBody, VideoDescriptionMusicSection, VideoDescriptionInfocardsSection ]);
+    this.items = Parser.parseArray(data.items, [VideoDescriptionHeader, ExpandableVideoDescriptionBody, VideoDescriptionMusicSection, VideoDescriptionInfocardsSection, HorizontalCardList ]);
   }
 }
