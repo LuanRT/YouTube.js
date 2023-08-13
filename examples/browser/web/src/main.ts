cb563c29abbb854e9a30204af9ab92859d9da68a
LuanRT <luan.lrt4@gmail.com>
2023-08-12 23:02:42 -0300

    chore: use Shaka for the browser example

--- examples/browser/web/src/main.ts
+++ examples/browser/web/src/main.ts
@@ -1,27 +1,27 @@
-import './style.css';
 import { Innertube, UniversalCache } from '../../../../bundle/browser';
-import dashjs from 'dashjs';
 
-const description = document.getElementById('description') as HTMLDivElement;
-const form = document.querySelector('form') as HTMLFormElement;
+// @ts-ignore - Shaka's TS support is not the best.
+import shaka from 'shaka-player/dist/shaka-player.ui.js';
+
+import "shaka-player/dist/controls.css";
+
 const title = document.getElementById('title') as HTMLHeadingElement;
+const description = document.getElementById('description') as HTMLDivElement;
 const metadata = document.getElementById('metadata') as HTMLDivElement;
 const loader = document.getElementById('loader') as HTMLDivElement;
-const video = document.getElementById('video') as HTMLVideoElement;
-const video_container = document.getElementById('video_container') as HTMLDivElement;
+const form = document.querySelector('form') as HTMLFormElement;
 
 async function main() {
   const yt = await Innertube.create({
     generate_session_locally: true,
     fetch: async (input: RequestInfo | URL, init?: RequestInit) => {
-      // url
       const url = typeof input === 'string'
         ? new URL(input)
         : input instanceof URL
           ? input
           : new URL(input.url);
 
-      // transform the url for use with our proxy
+      // Transform the url for use with our proxy.
       url.searchParams.set('__host', url.host);
       url.host = 'localhost:8080';
       url.protocol = 'http';
@@ -32,13 +32,15 @@ async function main() {
           ? input.headers
           : new Headers();
 
-      // now serialize the headers
+      // Now serialize the headers.
       url.searchParams.set('__headers', JSON.stringify([...headers]));
 
-      // @ts-ignore
-      input.duplex = 'half';
+      if (input instanceof Request) {
+        // @ts-ignore
+        input.duplex = 'half';
+      }
 
-      // copy over the request
+      // Copy over the request.
       const request = new Request(
         url,
         input instanceof Request ? input : undefined,
@@ -46,7 +48,6 @@ async function main() {
 
       headers.delete('user-agent');
 
-      // fetch the url
       return fetch(request, init ? {
         ...init,
         headers
@@ -60,45 +61,46 @@ async function main() {
   form.animate({ opacity: [0, 1] }, { duration: 300, easing: 'ease-in-out' });
   form.style.display = 'block';
 
-  showUI(false);
+  showUI({ hidePlayer: true });
 
-  let player: dashjs.MediaPlayerClass | undefined;
+  let player: shaka.Player | undefined;
+  let ui: shaka.ui.Overlay | undefined;
 
   form.addEventListener('submit', async (e) => {
     e.preventDefault();
 
     if (player) {
-      player.reset();
+      player.destroy();
     }
 
     hideUI();
 
-    let video_id;
+    let videoId;
 
-    const video_id_or_url = document.querySelector<HTMLInputElement>('input[type=text]')?.value;
+    const videoIdOrURL = document.querySelector<HTMLInputElement>('input[type=text]')?.value;
 
-    if (!video_id_or_url) {
+    if (!videoIdOrURL) {
       title.textContent = 'No video id or URL provided';
-      showUI(false);
+      showUI({ hidePlayer: true });
       return;
     }
 
     try {
-      if (video_id_or_url.match(/(http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/)) {
-        const endpoint = await yt.resolveURL(video_id_or_url);
+      if (videoIdOrURL.match(/(http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/)) {
+        const endpoint = await yt.resolveURL(videoIdOrURL);
 
         if (!endpoint.payload.videoId) {
           title.textContent = 'Could not resolve URL';
-          showUI(false);
+          showUI({ hidePlayer: true });
           return;
         }
 
-        video_id = endpoint.payload.videoId;
+        videoId = endpoint.payload.videoId;
       } else {
-        video_id = video_id_or_url;
+        videoId = videoIdOrURL;
       }
 
-      const info = await yt.getInfo(video_id);
+      const info = await yt.getInfo(videoId);
 
       title.textContent = info.basic_info.title || null;
       description.innerHTML = info.secondary_info?.description.toHTML() || '';
@@ -106,12 +108,12 @@ async function main() {
 
       document.title = info.basic_info.title || '';
 
-      metadata!.innerHTML = '';
-      metadata!.innerHTML += `<div class="metadata_item">${info.primary_info?.published.toHTML()}</div>`;
-      metadata!.innerHTML += `<div class="metadata_item">${info.primary_info?.view_count.toHTML()}</div>`;
-      metadata!.innerHTML += `<div class="metadata_item">${info.basic_info.like_count} likes</div>`;
+      metadata.innerHTML = '';
+      metadata.innerHTML += `<div id="metadata-item">${info.primary_info?.published.toHTML()}</div>`;
+      metadata.innerHTML += `<div id="metadata-item">${info.primary_info?.view_count.toHTML()}</div>`;
+      metadata.innerHTML += `<div id="metadata-item">${info.basic_info.like_count} likes</div>`;
 
-      showUI(true);
+      showUI({ hidePlayer: false });
 
       const dash = await info.toDash((url) => {
         url.searchParams.set('__host', url.host);
@@ -122,35 +124,155 @@ async function main() {
 
       const uri = 'data:application/dash+xml;charset=utf-8;base64,' + btoa(dash);
 
-      // create and append video element
-      const video_element = document.querySelector('video') as HTMLVideoElement;
-      video_element.setAttribute('controls', 'true');
-      video_element.poster = info.basic_info.thumbnail![0].url;
-
-      // use dash.js to parse the manifest
       if (player) {
-        player.destroy();
+        await player.destroy();
+        player = undefined;
       }
 
-      player = dashjs.MediaPlayer().create();
-      player.initialize(video_element, uri, true);
-      player.setInitialMediaSettingsFor('audio', { lang: 'en-US' });
+      if (ui) {
+        ui.destroy();
+        ui = undefined;
+      }
+
+      const videoEl = document.getElementById('videoel') as HTMLVideoElement;
+      const shakaContainer = document.getElementById('shaka-container') as HTMLDivElement;
+
+      shakaContainer
+        .querySelectorAll("div")
+        .forEach(node => node.remove());
+
+      shaka.polyfill.installAll();
+
+      if (shaka.Player.isBrowserSupported()) {
+        videoEl.poster = info.basic_info.thumbnail![0].url;
+
+        player = new shaka.Player(videoEl);
+        ui = new shaka.ui.Overlay(player, shakaContainer, videoEl);
+
+        const config = {
+          seekBarColors: {
+            base: 'rgba(255,255,255,.2)',
+            buffered: 'rgba(255,255,255,.4)',
+            played: 'rgb(255,0,0)',
+          },
+          fadeDelay: 0,
+        };
+
+        ui.configure(config);
+
+        const overflowMenuButton = document.querySelector('.shaka-overflow-menu-button');
+        if (overflowMenuButton) {
+          overflowMenuButton.innerHTML = 'settings';
+        }
+
+        const backToOverflowButton = document.querySelector('.shaka-back-to-overflow-button .material-icons-round');
+        if (backToOverflowButton) {
+          backToOverflowButton.innerHTML = 'arrow_back_ios_new';
+        }
+
+        player.configure({
+          streaming: {
+            bufferingGoal: 180,
+            rebufferingGoal: 0.02,
+            bufferBehind: 300
+          }
+        });
+
+        player.getNetworkingEngine()?.registerRequestFilter((_type: any, request: any) => {
+          const uri = request.uris[0];
+          const url = new URL(uri);
+          const headers = request.headers;
+
+          request.method = 'POST';
+
+          // protobuf - { 15: 0 }
+          request.body = new Uint8Array([120, 0]);
+
+          if (url.pathname === "/videoplayback") {
+            if (headers.Range) {
+              request.headers = {};
+              url.searchParams.set("range", headers.Range.split("=")[1]);
+              url.searchParams.set("alr", "yes");
+            }
+          }
+
+          request.uris[0] = url.toString();
+        });
+
+        // The UTF-8 characters "h", "t", "t", and "p".
+        const HTTP_IN_HEX = 0x68747470;
+
+        const RequestType = shaka.net.NetworkingEngine.RequestType;
+
+        player.getNetworkingEngine()?.registerResponseFilter(async (type: any, response: any) => {
+          const dataView = new DataView(response.data);
+          
+          if (response.data.byteLength < 4 ||
+            dataView.getUint32(0) != HTTP_IN_HEX) {
+            // This doesn't start with "http", so it is not an ALR.
+            return;
+          }
+
+          // Interpret the response data as a URL string.
+          const response_as_string = shaka.util.StringUtils.fromUTF8(response.data);
+
+          let retry_parameters;
+
+          if (type == RequestType.MANIFEST) {
+            retry_parameters = player!.getConfiguration().manifest.retryParameters;
+          } else if (type == RequestType.SEGMENT) {
+            retry_parameters = player!.getConfiguration().streaming.retryParameters;
+          } else if (type == RequestType.LICENSE) {
+            retry_parameters = player!.getConfiguration().drm.retryParameters;
+          } else {
+            retry_parameters = shaka.net.NetworkingEngine.defaultRetryParameters();
+          }
+
+          // Make another request for the redirect URL.
+          const uris = [response_as_string];
+          const redirect_request = shaka.net.NetworkingEngine.makeRequest(uris, retry_parameters);
+          const request_operation = player!.getNetworkingEngine()!.request(type, redirect_request);
+          const redirect_response = await request_operation.promise;
+
+          // Modify the original response to contain the results of the redirect
+          // response.
+          response.data = redirect_response.data;
+          response.headers = redirect_response.headers;
+          response.uri = redirect_response.uri;
+        });
+
+        try {
+          await player.load(uri);
+        } catch (e) {
+          console.error('Could not load manifest', e);
+        }
+      } else {
+        console.error('Browser not supported!');
+      }
     } catch (error) {
       title.textContent = 'An error occurred (see console)';
-      showUI(false);
+      showUI({ hidePlayer: true });
       console.error(error);
     }
   });
 }
 
-function showUI(with_video = true) {
-  loader.style.display = 'none';
-  video.style.display = with_video ? 'block' : 'none';
+function showUI(args: { hidePlayer?: boolean } = {
+  hidePlayer: true,
+}) {
+  const ytplayer = document.getElementById('shaka-container') as HTMLDivElement;
+
+  ytplayer.style.display = args.hidePlayer ? 'none' : 'block';
+
+  const video_container = document.getElementById('video-container') as HTMLDivElement;
   video_container.animate({ opacity: [0, 1] }, { duration: 300, easing: 'ease-in-out' });
   video_container.style.display = 'block';
+
+  loader.style.display = 'none';
 }
 
 function hideUI() {
+  const video_container = document.getElementById('video-container') as HTMLDivElement;
   video_container.style.display = 'none';
   loader.style.display = 'block';
 }
