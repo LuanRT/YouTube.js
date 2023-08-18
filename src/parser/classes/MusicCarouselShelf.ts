f7666051f66563ccfa64b21f2ecd7815082a0a4b
LuanRT <luan.lrt4@gmail.com>
2023-08-18 08:11:04 -0300

    chore(parser): update `MusicCarouselShelf`

--- src/parser/classes/MusicCarouselShelf.ts
+++ src/parser/classes/MusicCarouselShelf.ts
@@ -2,6 +2,7 @@ import { YTNode, type ObservedArray } from '../helpers.js';
 import Parser, { type RawNode } from '../index.js';
 
 import MusicCarouselShelfBasicHeader from './MusicCarouselShelfBasicHeader.js';
+import MusicMultiRowListItem from './MusicMultiRowListItem.js';
 import MusicNavigationButton from './MusicNavigationButton.js';
 import MusicResponsiveListItem from './MusicResponsiveListItem.js';
 import MusicTwoRowItem from './MusicTwoRowItem.js';
@@ -10,13 +11,13 @@ export default class MusicCarouselShelf extends YTNode {
   static type = 'MusicCarouselShelf';
 
   header: MusicCarouselShelfBasicHeader | null;
-  contents: ObservedArray<MusicTwoRowItem | MusicResponsiveListItem | MusicNavigationButton>;
+  contents: ObservedArray<MusicTwoRowItem | MusicResponsiveListItem | MusicMultiRowListItem | MusicNavigationButton>;
   num_items_per_column?: number;
 
   constructor(data: RawNode) {
     super();
     this.header = Parser.parseItem(data.header, MusicCarouselShelfBasicHeader);
-    this.contents = Parser.parseArray(data.contents, [ MusicTwoRowItem, MusicResponsiveListItem, MusicNavigationButton ]);
+    this.contents = Parser.parseArray(data.contents, [MusicTwoRowItem, MusicResponsiveListItem, MusicMultiRowListItem, MusicNavigationButton ]);
 
     if (Reflect.has(data, 'numItemsPerColumn')) {
       this.num_items_per_column = parseInt(data.numItemsPerColumn);
