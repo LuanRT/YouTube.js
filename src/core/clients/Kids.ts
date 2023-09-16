64319f607b46e7d4cedc111ea2194909c011c91b
rsandbach <rsandbach@gmail.com>
2023-09-16 00:22:55 -0400

    Fix whitepsace issues.

--- src/core/clients/Kids.ts
+++ src/core/clients/Kids.ts
@@ -85,36 +85,35 @@ export default class Kids {
     return new HomeFeed(this.#session.actions, response);
   }
 
-    /**
-   * Retrieves the list of supervised accounts that the signed-in user has 
+  /**
+   * Retrieves the list of supervised accounts that the signed-in user has
    * access to, and blocks the given channel for each of them.
    * @param channel_id - The channel id to block.
    * @returns A list of API responses.
    */
-    async blockChannel(channel_id: string): Promise<ApiResponse[]> {
-      if (!this.#session.logged_in)
-        throw new InnertubeError('You must be signed in to perform this operation.');
-  
-      const blocklist_payload = KidsBlocklistPickerEndpoint.build({ channel_id: channel_id });
-      const response = (await this.#session.actions.execute( KidsBlocklistPickerEndpoint.PATH, blocklist_payload ));     
-      const popup = response.data.command.confirmDialogEndpoint    
-      const popupFragment = { contents: popup.content, engagementPanels: [] };
-      const kidPicker = Parser.parseResponse(popupFragment);
-      const kids = kidPicker.contents_memo?.getType(KidsBlocklistPickerItem)
-      
-      if (!kids)
-        throw new InnertubeError('Could not find any kids profiles or supervised accounts.');
-  
-      // Iterate through the kids and block the channel if not already blocked.
-      var responses: ApiResponse[] = [];
+  async blockChannel(channel_id: string): Promise<ApiResponse[]> {
+    if (!this.#session.logged_in)
+      throw new InnertubeError('You must be signed in to perform this operation.');
+
+    const blocklist_payload = KidsBlocklistPickerEndpoint.build({ channel_id: channel_id });
+    const response = (await this.#session.actions.execute( KidsBlocklistPickerEndpoint.PATH, blocklist_payload ));
+    const popup = response.data.command.confirmDialogEndpoint;
+    const popupFragment = { contents: popup.content, engagementPanels: [] };
+    const kidPicker = Parser.parseResponse(popupFragment);
+    const kids = kidPicker.contents_memo?.getType(KidsBlocklistPickerItem);
+
+    if (!kids)
+      throw new InnertubeError('Could not find any kids profiles or supervised accounts.');
+
+    // Iterate through the kids and block the channel if not already blocked.
+    var responses: ApiResponse[] = [];
       for (const kid of kids) {
-        if(!kid.block_button?.is_toggled) {            
-          kid.setActions(this.#session.actions);
-          // Block channel and add to the response list. 
-          responses.push(await kid.blockchannel());     
-        }
+      if(!kid.block_button?.is_toggled) {
+        kid.setActions(this.#session.actions);
+        // Block channel and add to the response list.
+        responses.push(await kid.blockchannel());
       }
-  
-      return responses;
     }
+    return responses;
+  }
 }
