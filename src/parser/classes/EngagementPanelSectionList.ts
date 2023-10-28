0796ada4fc2630059649c4b0735e51dd07325783
JellyBrick <shlee1503@naver.com>
2023-10-29 04:54:51 +0900

    feat: add `VideoAttributeView`

--- src/parser/classes/EngagementPanelSectionList.ts
+++ src/parser/classes/EngagementPanelSectionList.ts
@@ -6,12 +6,13 @@ import MacroMarkersList from './MacroMarkersList.js';
 import ProductList from './ProductList.js';
 import SectionList from './SectionList.js';
 import StructuredDescriptionContent from './StructuredDescriptionContent.js';
+import VideoAttributeView from './VideoAttributeView.js';
 
 export default class EngagementPanelSectionList extends YTNode {
   static type = 'EngagementPanelSectionList';
 
   header: EngagementPanelTitleHeader | null;
-  content: SectionList | ContinuationItem | StructuredDescriptionContent | MacroMarkersList | ProductList | null;
+  content: VideoAttributeView | ContinuationItem | StructuredDescriptionContent | MacroMarkersList | ProductList | null;
   target_id?: string;
   panel_identifier?: string;
   visibility?: string;
@@ -19,7 +20,7 @@ export default class EngagementPanelSectionList extends YTNode {
   constructor(data: RawNode) {
     super();
     this.header = Parser.parseItem(data.header, EngagementPanelTitleHeader);
-    this.content = Parser.parseItem(data.content, [ SectionList, ContinuationItem, StructuredDescriptionContent, MacroMarkersList, ProductList ]);
+    this.content = Parser.parseItem(data.content, [ VideoAttributeView, SectionList, ContinuationItem, StructuredDescriptionContent, MacroMarkersList, ProductList ]);
     this.panel_identifier = data.panelIdentifier;
     this.target_id = data.targetId;
     this.visibility = data.visibility;
---
+++ src/parser/classes/VideoAttributeView.ts
@@ -0,0 +1,56 @@
+import { YTNode } from '../helpers.js';
+import { NavigationEndpoint } from '../nodes.js';
+
+import ContentPreviewImageView from './ContentPreviewImageView.js';
+import Parser from '../index.js';
+
+import type { RawNode } from '../types/index.js';
+
+export default class VideoAttributeView extends YTNode {
+  static type = 'VideoAttributeView';
+
+  image: ContentPreviewImageView | null;
+  image_style: string;
+  title: string;
+  subtitle: string;
+  secondary_subtitle: {
+    content: string
+  };
+  orientation: string;
+  sizing_rule: string;
+  overflow_menu_on_tap: {
+    innertube_command: NavigationEndpoint
+  };
+  overflow_menu_a11y_label: string;
+  logging_directives: {
+    tracking_params: string,
+    visibility: {
+      types: string
+    },
+    enable_displaylogger_experiment: boolean
+  };
+
+  constructor(data: RawNode) {
+    super();
+    this.image = Parser.parseItem(data.image, ContentPreviewImageView);
+    this.image_style = data.imageStyle;
+    this.title = data.title;
+    this.subtitle = data.subtitle;
+    this.secondary_subtitle = {
+      content: data.secondarySubtitle.content
+    };
+    this.orientation = data.orientation;
+    this.sizing_rule = data.sizingRule;
+    this.overflow_menu_on_tap = {
+      innertube_command: new NavigationEndpoint(data.overflowMenuOnTap.innertubeCommand)
+    };
+    this.overflow_menu_a11y_label = data.overflowMenuA11yLabel;
+    this.logging_directives = {
+      tracking_params: data.loggingDirectives.trackingParams,
+      visibility: {
+        types: data.loggingDirectives.visibility.types
+      },
+      enable_displaylogger_experiment: data.loggingDirectives.enableDisplayloggerExperiment
+    };
+  }
+}
