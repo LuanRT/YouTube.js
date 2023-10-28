5485460ffced7ba141298154d5e54e1e8a214d2b
JellyBrick <shlee1503@naver.com>
2023-10-29 05:38:10 +0900

    feat: add `FeedNudge`
    see https://github.com/LuanRT/YouTube.js/actions/runs/6679090140/job/18150827068?pr=532

---
+++ src/parser/classes/FeedNudge.ts
@@ -0,0 +1,24 @@
+import { YTNode } from '../helpers.js';
+import { NavigationEndpoint } from '../nodes.js';
+import type { RawNode } from '../types/index.js';
+
+export default class FeedNudge extends YTNode {
+  static type = 'FeedNudge';
+
+  title: Text;
+  subtitle: Text;
+  endpoint: NavigationEndpoint;
+  apply_modernized_style: boolean;
+  trim_style: string;
+  background_style: string;
+
+  constructor(data: RawNode) {
+    super();
+    this.title = new Text(data.title);
+    this.subtitle = new Text(data.subtitle);
+    this.endpoint = new NavigationEndpoint(data.impressionEndpoint);
+    this.apply_modernized_style = data.applyModernizedStyle;
+    this.trim_style = data.trimStyle;
+    this.background_style = data.backgroundStyle;
+  }
+}
