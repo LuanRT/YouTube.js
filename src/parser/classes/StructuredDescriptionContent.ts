ee79dfe73c98a1c1bff67a169a44909e8493a86a
rsandbach <rsandbach@gmail.com>
2023-11-06 22:07:04 -0500

    Fixed failing test.

--- src/parser/classes/StructuredDescriptionContent.ts
+++ src/parser/classes/StructuredDescriptionContent.ts
@@ -1,11 +1,11 @@
 import { YTNode, type ObservedArray } from '../helpers.js';
-import Parser, { type RawNode } from '../index.js';
+import { Parser, type RawNode } from '../index.js';
 import ExpandableVideoDescriptionBody from './ExpandableVideoDescriptionBody.js';
 import HorizontalCardList from './HorizontalCardList.js';
 import VideoDescriptionHeader from './VideoDescriptionHeader.js';
 import VideoDescriptionInfocardsSection from './VideoDescriptionInfocardsSection.js';
 import VideoDescriptionMusicSection from './VideoDescriptionMusicSection.js';
-import type VideoDescriptionTranscriptSection from './VideoDescriptionTranscriptSection.js';
+import VideoDescriptionTranscriptSection from './VideoDescriptionTranscriptSection.js';
 
 export default class StructuredDescriptionContent extends YTNode {
   static type = 'StructuredDescriptionContent';
@@ -17,6 +17,7 @@ export default class StructuredDescriptionContent extends YTNode {
 
   constructor(data: RawNode) {
     super();
-    this.items = Parser.parseArray(data.items, [ VideoDescriptionHeader, ExpandableVideoDescriptionBody, VideoDescriptionMusicSection, VideoDescriptionInfocardsSection, HorizontalCardList ]);
+    this.items = Parser.parseArray(data.items, [ VideoDescriptionHeader, ExpandableVideoDescriptionBody, VideoDescriptionMusicSection
+      , VideoDescriptionInfocardsSection, VideoDescriptionTranscriptSection, HorizontalCardList ]);
   }
 }
