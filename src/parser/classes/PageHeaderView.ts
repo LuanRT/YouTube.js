17919175e3bd28bbd7a418bcadc8177d87cc3874
Brahim Hadriche <brahim.hadriche@gmail.com>
2023-11-26 22:57:14 -0500

    Update PageHeaderView

--- src/parser/classes/PageHeaderView.ts
+++ src/parser/classes/PageHeaderView.ts
@@ -1,17 +1,18 @@
 import { YTNode } from '../helpers.js';
 import Parser, { type RawNode } from '../index.js';
 import ContentPreviewImageView from './ContentPreviewImageView.js';
+import DecoratedAvatarView from './DecoratedAvatarView.js';
 import DynamicTextView from './DynamicTextView.js';
 
 export default class PageHeaderView extends YTNode {
   static type = 'PageHeaderView';
 
-  image: ContentPreviewImageView | null;
+  image: ContentPreviewImageView | DecoratedAvatarView | null;
   title: DynamicTextView | null;
 
   constructor(data: RawNode) {
     super();
-    this.image = Parser.parseItem(data.image, ContentPreviewImageView);
+    this.image = Parser.parseItem(data.image, [ ContentPreviewImageView, DecoratedAvatarView ]);
     this.title = Parser.parseItem(data.title, DynamicTextView);
   }
 }
