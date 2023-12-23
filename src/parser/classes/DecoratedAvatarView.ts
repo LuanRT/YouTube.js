1aa827c86bce1496ff972311803108ee81e48352
Brahim Hadriche <brahim.hadriche@gmail.com>
2023-12-23 14:46:50 -0500

    Apply suggestions from code review
    Co-authored-by: absidue <48293849+absidue@users.noreply.github.com>

--- src/parser/classes/DecoratedAvatarView.ts
+++ src/parser/classes/DecoratedAvatarView.ts
@@ -1,35 +1,19 @@
 import { YTNode } from '../helpers.js';
 import type { RawNode } from '../index.js';
-import { NavigationEndpoint } from '../nodes.js';
+import NavigationEndpoint from './NavigationEndpoint.js';
 import AvatarView from './AvatarView.js';
 
 export default class DecoratedAvatarView extends YTNode {
   static type = 'DecoratedAvatarView';
 
-  avatar: {
-      avatar_view_model: AvatarView
-    };
+  avatar: AvatarView;
   a11y_label: string;
-  renderer_context: {
-      command_context: {
-        on_tap: {
-          innertube_command: NavigationEndpoint
-        }
-      }
-    };
+  on_tap_endpoint: NavigationEndpoint;
 
   constructor(data: RawNode) {
     super();
-    this.avatar = {
-      avatar_view_model: new AvatarView(data.avatar.avatarViewModel)
-    };
+    this.avatar = new AvatarView(data.avatar.avatarViewModel);
     this.a11y_label = data.a11yLabel;
-    this.renderer_context = {
-      command_context: {
-        on_tap: {
-          innertube_command: new NavigationEndpoint(data.rendererContext.commandContext.onTap.innertubeCommand)
-        }
-      }
-    };
+    this.on_tap_endpoint = new NavigationEndpoint(data.rendererContext.commandContext.onTap.innertubeCommand);
   }
 }
