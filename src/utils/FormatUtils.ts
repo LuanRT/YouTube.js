a19511de24bb82007aab072844efe64bbb8698da
슈리튬 <9804cjh@naver.com>
2024-06-29 04:45:39 +0900

    fix(FormatUtils): Throw an error if download requests fails

--- src/utils/FormatUtils.ts
+++ src/utils/FormatUtils.ts
@@ -90,6 +90,10 @@ export async function download(
             signal: cancel.signal
           });
 
+          // Throw if the response is not 2xx
+          if (!response.ok)
+            throw new InnertubeError('The server responded with a non 2xx status code', { error_type: 'FETCH_FAILED', response });
+
           const body = response.body;
 
           if (!body)
