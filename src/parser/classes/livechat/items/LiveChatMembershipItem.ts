fdde645e8cd4d174dd937fd05411dfefe8864453
Riki Nozomu <rikinozomu2022@gmail.com>
2024-07-01 03:03:03 +0700

    added primary text & message for LiveChatMembershipItem

--- src/parser/classes/livechat/items/LiveChatMembershipItem.ts
+++ src/parser/classes/livechat/items/LiveChatMembershipItem.ts
@@ -9,7 +9,9 @@ export default class LiveChatMembershipItem extends YTNode {
 
   id: string;
   timestamp: number;
+  header_primary_text: Text;
   header_subtext: Text;
+  message: Text;
   author: Author;
   menu_endpoint: NavigationEndpoint;
 
@@ -18,6 +20,8 @@ export default class LiveChatMembershipItem extends YTNode {
     this.id = data.id;
     this.timestamp = Math.floor(parseInt(data.timestampUsec) / 1000);
     this.header_subtext = new Text(data.headerSubtext);
+    this.header_primary_text = new Text(data.headerPrimaryText);
+    this.message = new Text(data.message);
     this.author = new Author(data.authorName, data.authorBadges, data.authorPhoto, data.authorExternalChannelId);
     this.menu_endpoint = new NavigationEndpoint(data.contextMenuEndpoint);
   }
