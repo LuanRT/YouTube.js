8bcfdea65d289cca2977117dc7862b490298483a
Luan <luan.lrt4@gmail.com>
2024-07-10 02:10:53 -0300

    fix: bump Jinter to v2

--- src/platform/jsruntime/jinter.ts
+++ src/platform/jsruntime/jinter.ts
@@ -7,13 +7,13 @@ const TAG = 'JsRuntime';
 export default function evaluate(code: string, env: Record<string, VMPrimative>) {
   Log.debug(TAG, 'Evaluating JavaScript:\n', code);
 
-  const runtime = new Jinter(code);
+  const runtime = new Jinter();
 
   for (const [ key, value ] of Object.entries(env)) {
     runtime.scope.set(key, value);
   }
 
-  const result = runtime.interpret();
+  const result = runtime.evaluate(code);
 
   Log.debug(TAG, 'Done. Result:', result);
 
