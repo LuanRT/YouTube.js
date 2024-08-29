f7af7a80afe63a06b4866db4071ad26d5f65cad6
Dave Nicolson <david.nicolson@gmail.com>
2024-08-29 21:10:01 +0200

    Fix FeedFilterChipBar parse error

--- src/parser/classes/ItemSection.ts
+++ src/parser/classes/ItemSection.ts
@@ -4,18 +4,19 @@ import ItemSectionHeader from './ItemSectionHeader.js';
 import ItemSectionTabbedHeader from './ItemSectionTabbedHeader.js';
 import CommentsHeader from './comments/CommentsHeader.js';
 import SortFilterHeader from './SortFilterHeader.js';
+import FeedFilterChipBar from './FeedFilterChipBar.js';
 
 export default class ItemSection extends YTNode {
   static type = 'ItemSection';
 
-  header: CommentsHeader | ItemSectionHeader | ItemSectionTabbedHeader | SortFilterHeader | null;
+  header: CommentsHeader | ItemSectionHeader | ItemSectionTabbedHeader | SortFilterHeader | FeedFilterChipBar | null;
   contents: ObservedArray<YTNode>;
   target_id?: string;
   continuation?: string;
 
   constructor(data: RawNode) {
     super();
-    this.header = Parser.parseItem(data.header, [ CommentsHeader, ItemSectionHeader, ItemSectionTabbedHeader, SortFilterHeader ]);
+    this.header = Parser.parseItem(data.header, [ CommentsHeader, ItemSectionHeader, ItemSectionTabbedHeader, SortFilterHeader, FeedFilterChipBar ]);
     this.contents = Parser.parseArray(data.contents);
 
     if (data.targetId || data.sectionIdentifier) {
