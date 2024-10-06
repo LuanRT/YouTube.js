ff2e167c5895bc6173a02adb590cea8fedf9a70e
Emilien Devos <4016501+unixfox@users.noreply.github.com>
2024-10-06 18:31:40 +0200

    fix Import assertions are deprecated

--- src/platform/deno.ts
+++ src/platform/deno.ts
@@ -3,7 +3,7 @@ import type { ICache } from '../types/Cache.js';
 import { Platform } from '../utils/Utils.js';
 import evaluate from './jsruntime/jinter.js';
 import sha1Hash from './polyfills/web-crypto.js';
-import package_json from '../../package.json' assert { type: 'json' };
+import package_json from '../../package.json' with { type: 'json' };
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
