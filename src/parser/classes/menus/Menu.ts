f5050c613b1dafebedea3f28bc916ea0d5522e8e
absidue <48293849+absidue@users.noreply.github.com>
2024-10-28 19:48:06 +0100

    fix(VideoInfo): Fix like count being undefined

--- src/parser/classes/menus/Menu.ts
+++ src/parser/classes/menus/Menu.ts
@@ -3,18 +3,20 @@ import type { ObservedArray } from '../../helpers.js';
 import { YTNode } from '../../helpers.js';
 import type { RawNode } from '../../index.js';
 import Button from '../Button.js';
+import ButtonView from '../ButtonView.js';
+import SegmentedLikeDislikeButtonView from '../SegmentedLikeDislikeButtonView.js';
 
 export default class Menu extends YTNode {
   static type = 'Menu';
 
   items: ObservedArray<YTNode>;
-  top_level_buttons: ObservedArray<Button>;
+  top_level_buttons: ObservedArray<Button | ButtonView | SegmentedLikeDislikeButtonView>;
   label?: string;
 
   constructor(data: RawNode) {
     super();
     this.items = Parser.parseArray(data.items);
-    this.top_level_buttons = Parser.parseArray(data.topLevelButtons, Button);
+    this.top_level_buttons = Parser.parseArray(data.topLevelButtons, [ Button, ButtonView, SegmentedLikeDislikeButtonView ]);
 
     if (Reflect.has(data, 'accessibility') && Reflect.has(data.accessibility, 'accessibilityData')) {
       this.label = data.accessibility.accessibilityData.label;
