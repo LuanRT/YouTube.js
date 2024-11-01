9d9011c5405298c2ca1f212677352a953dc1511f
jonz94 <jody16888@gmail.com>
2024-11-02 04:18:17 +0800

    refactor: revert `Author` logic

--- src/parser/classes/livechat/items/LiveChatSponsorshipsGiftPurchaseAnnouncement.ts
+++ src/parser/classes/livechat/items/LiveChatSponsorshipsGiftPurchaseAnnouncement.ts
@@ -1,6 +1,5 @@
 import { YTNode } from '../../../helpers.js';
 import { Parser, type RawNode } from '../../../index.js';
-import Author from '../../misc/Author.js';
 import LiveChatSponsorshipsHeader from './LiveChatSponsorshipsHeader.js';
 
 export default class LiveChatSponsorshipsGiftPurchaseAnnouncement extends YTNode {
@@ -8,21 +7,14 @@ export default class LiveChatSponsorshipsGiftPurchaseAnnouncement extends YTNode
 
   id: string;
   timestamp_usec: string;
-  author: Author;
+  author_external_channel_id: string;
   header: LiveChatSponsorshipsHeader | null;
 
   constructor(data: RawNode) {
     super();
     this.id = data.id;
     this.timestamp_usec = data.timestampUsec;
-
-    this.author = new Author(
-      data.header.liveChatSponsorshipsHeaderRenderer.authorName,
-      data.header.liveChatSponsorshipsHeaderRenderer.authorBadges,
-      data.header.liveChatSponsorshipsHeaderRenderer.authorPhoto,
-      data.authorExternalChannelId
-    );
-
+    this.author_external_channel_id = data.authorExternalChannelId;
     this.header = Parser.parseItem(data.header, LiveChatSponsorshipsHeader);
   }
 }
