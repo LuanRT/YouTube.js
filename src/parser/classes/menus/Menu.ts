80dbd6fe711598446ca27fb7c73b281ed2a2ad94
Luan <luan.lrt4@gmail.com>
2024-11-15 03:07:27 -0300

    chore(Menu): Update `flexible_items` type

--- src/parser/classes/menus/Menu.ts
+++ src/parser/classes/menus/Menu.ts
@@ -5,19 +5,20 @@ import type { RawNode } from '../../index.js';
 import Button from '../Button.js';
 import ButtonView from '../ButtonView.js';
 import SegmentedLikeDislikeButtonView from '../SegmentedLikeDislikeButtonView.js';
+import MenuFlexibleItem from './MenuFlexibleItem.js';
 
 export default class Menu extends YTNode {
   static type = 'Menu';
 
   public items: ObservedArray<YTNode>;
-  public flexible_items: ObservedArray<YTNode>;
+  public flexible_items: ObservedArray<MenuFlexibleItem>;
   public top_level_buttons: ObservedArray<Button | ButtonView | SegmentedLikeDislikeButtonView>;
   public label?: string;
 
   constructor(data: RawNode) {
     super();
     this.items = Parser.parseArray(data.items);
-    this.flexible_items = Parser.parseArray(data.flexibleItems);
+    this.flexible_items = Parser.parseArray(data.flexibleItems, MenuFlexibleItem);
     this.top_level_buttons = Parser.parseArray(data.topLevelButtons, [ Button, ButtonView, SegmentedLikeDislikeButtonView ]);
 
     if (Reflect.has(data, 'accessibility') && Reflect.has(data.accessibility, 'accessibilityData')) {
