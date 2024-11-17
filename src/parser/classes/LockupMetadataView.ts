0affbf8dd2e53ee93fdecc15393f7fdde6b2b46c
absidue <48293849+absidue@users.noreply.github.com>
2024-11-17 23:25:15 +0100

    feat(parser): Add optional image property to LockupMetadataView

--- src/parser/classes/LockupMetadataView.ts
+++ src/parser/classes/LockupMetadataView.ts
@@ -1,6 +1,7 @@
 import { YTNode } from '../helpers.js';
 import { Parser, type RawNode } from '../index.js';
 import ContentMetadataView from './ContentMetadataView.js';
+import DecoratedAvatarView from './DecoratedAvatarView.js';
 import Text from './misc/Text.js';
 
 export default class LockupMetadataView extends YTNode {
@@ -8,11 +9,13 @@ export default class LockupMetadataView extends YTNode {
 
   title: Text;
   metadata: ContentMetadataView | null;
+  image: DecoratedAvatarView | null;
 
   constructor(data: RawNode) {
     super();
 
     this.title = Text.fromAttributed(data.title);
     this.metadata = Parser.parseItem(data.metadata, ContentMetadataView);
+    this.image = Parser.parseItem(data.image, DecoratedAvatarView);
   }
 }
