7c18772a6d9be01e56fb2389d59e80b7bd1de239
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2024-11-17 13:05:11 -0500

    feat(Parser): add support for parsing subtitle for `RichShelf`

--- src/parser/classes/RichShelf.ts
+++ src/parser/classes/RichShelf.ts
@@ -9,6 +9,7 @@ export default class RichShelf extends YTNode {
   title: Text;
   contents: ObservedArray<YTNode>;
   endpoint?: NavigationEndpoint;
+  subtitle?: Text;
 
   constructor(data: RawNode) {
     super();
@@ -17,6 +18,10 @@ export default class RichShelf extends YTNode {
 
     if (Reflect.has(data, 'endpoint')) {
       this.endpoint = new NavigationEndpoint(data.endpoint);
+    }
+
+    if (Reflect.has(data, 'subtitle')) {
+      this.subtitle = new Text(data.subtitle);
     }
   }
 }
