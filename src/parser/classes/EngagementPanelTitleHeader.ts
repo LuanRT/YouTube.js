af3a91645d84798e744519ec8f24e565cc1ecdb1
Luan <luan.lrt4@gmail.com>
2024-11-27 07:30:10 -0300

    feat(EngagementPanelTitleHeader): Add `contextual_info` and `menu`
    Both con contain info about the comments section.

--- src/parser/classes/EngagementPanelTitleHeader.ts
+++ src/parser/classes/EngagementPanelTitleHeader.ts
@@ -6,12 +6,16 @@ import Button from './Button.js';
 export default class EngagementPanelTitleHeader extends YTNode {
   static type = 'EngagementPanelTitleHeader';
 
-  title: Text;
-  visibility_button: Button | null;
+  public title: Text;
+  public visibility_button: Button | null;
+  public contextual_info?: Text;
+  public menu: YTNode | null;
 
   constructor(data: RawNode) {
     super();
     this.title = new Text(data.title);
+    this.contextual_info = data.contextualInfo ? new Text(data.contextualInfo) : undefined;
     this.visibility_button = Parser.parseItem(data.visibilityButton, Button);
+    this.menu = Parser.parseItem(data.menu);
   }
 }
