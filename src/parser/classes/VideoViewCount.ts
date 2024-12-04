d10fe6834a0d063d94b65289d54a52ed3398eff4
Luan <luan.lrt4@gmail.com>
2024-12-04 14:32:15 -0300

    feat(VideoViewCount): Add `extra_short_view_count` field

--- src/parser/classes/VideoViewCount.ts
+++ src/parser/classes/VideoViewCount.ts
@@ -7,12 +7,14 @@ export default class VideoViewCount extends YTNode {
 
   public original_view_count: string;
   public short_view_count: Text;
+  public extra_short_view_count: Text;
   public view_count: Text;
 
   constructor(data: RawNode) {
     super();
     this.original_view_count = data.originalViewCount;
     this.short_view_count = new Text(data.shortViewCount);
+    this.extra_short_view_count = new Text(data.extraShortViewCount);
     this.view_count = new Text(data.viewCount);
   }
 }
