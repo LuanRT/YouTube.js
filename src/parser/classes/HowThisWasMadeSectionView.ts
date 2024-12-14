0b73702bdd7870c793aa4097932267059fe88922
Luan <luan.lrt4@gmail.com>
2024-12-14 12:41:24 -0300

    chore(HowThisWasMadeSectionView): Make some fields optional
    Just in case.

--- src/parser/classes/HowThisWasMadeSectionView.ts
+++ src/parser/classes/HowThisWasMadeSectionView.ts
@@ -5,14 +5,17 @@ import Text from './misc/Text.js';
 export default class HowThisWasMadeSectionView extends YTNode {
   static type = 'HowThisWasMadeSectionView';
 
-  public section_title: Text;
-  public body_text: Text;
-  public body_header: Text;
+  public section_title?: Text;
+  public body_text?: Text;
+  public body_header?: Text;
 
   constructor(data: RawNode) {
     super();
-    this.section_title = Text.fromAttributed(data.sectionText);
-    this.body_text = Text.fromAttributed(data.bodyText);
-    this.body_header = Text.fromAttributed(data.bodyHeader);
+    if (Reflect.has(data, 'sectionText'))
+      this.section_title = Text.fromAttributed(data.sectionText);
+    if (Reflect.has(data, 'bodyText'))
+      this.body_text = Text.fromAttributed(data.bodyText);
+    if (Reflect.has(data, 'bodyHeader'))
+      this.body_header = Text.fromAttributed(data.bodyHeader);
   }
 }
