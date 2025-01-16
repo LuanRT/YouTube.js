a9e4ab3c9f964f4a4a212cc109dccb9b78b90c1a
Konstantin Späth <duell10111@t-online.de>
2025-01-16 16:22:09 +0100

    chore: remove tracking params

--- src/parser/classes/ReloadContinuationData.ts
+++ src/parser/classes/ReloadContinuationData.ts
@@ -5,11 +5,9 @@ export default class ReloadContinuationData extends YTNode {
   static type = 'ReloadContinuationData';
 
   continuation: string;
-  click_tracking_params: string;
 
   constructor(data: RawNode) {
     super();
     this.continuation = data.continuation;
-    this.click_tracking_params = data.clickTrackingParams;
   }
 }
