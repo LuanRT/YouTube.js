3cf2b56fc275c8be0f50df07dde38853144c1eb0
Dave Nicolson <david.nicolson@gmail.com>
2025-02-16 18:39:24 +0100

    Fix type mismatch

--- src/parser/classes/DialogView.ts
+++ src/parser/classes/DialogView.ts
@@ -3,18 +3,19 @@ import { Parser, type RawNode } from '../index.js';
 import DialogHeaderView from './DialogHeaderView.js';
 import FormFooterView from './FormFooterView.js';
 import CreatePlaylistDialogFormView from './CreatePlaylistDialogFormView.js';
+import PanelFooterView from './PanelFooterView.js';
 
 export default class DialogView extends YTNode {
   static type = 'DialogView';
 
   public header: DialogHeaderView | null;
-  public footer: FormFooterView | null;
+  public footer: FormFooterView | PanelFooterView | null;
   public custom_content: CreatePlaylistDialogFormView | null;
 
   constructor (data: RawNode) {
     super();
     this.header = Parser.parseItem(data.header, DialogHeaderView);
-    this.footer = Parser.parseItem(data.footer, FormFooterView);
+    this.footer = Parser.parseItem(data.footer, [ FormFooterView, PanelFooterView ]);
     this.custom_content = Parser.parseItem(data.customContent, CreatePlaylistDialogFormView);
   }
 }
