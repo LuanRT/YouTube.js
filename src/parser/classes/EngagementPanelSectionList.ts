bb85e44a9e64e261f94cca5840b568f28b64f8e6
Konstantin Späth <duell10111@t-online.de>
2025-03-22 17:34:20 +0100

    chore: add missing tv nodes

--- src/parser/classes/EngagementPanelSectionList.ts
+++ src/parser/classes/EngagementPanelSectionList.ts
@@ -9,12 +9,13 @@ import SectionList from './SectionList.js';
 import StructuredDescriptionContent from './StructuredDescriptionContent.js';
 import VideoAttributeView from './VideoAttributeView.js';
 import OverlayPanelHeader from './OverlayPanelHeader.js';
+import ItemSection from './ItemSection.js';
 
 export default class EngagementPanelSectionList extends YTNode {
   static type = 'EngagementPanelSectionList';
 
   header: EngagementPanelTitleHeader | OverlayPanelHeader | null;
-  content: VideoAttributeView | SectionList | ContinuationItem | ClipSection | StructuredDescriptionContent | MacroMarkersList | ProductList | null;
+  content: VideoAttributeView | ItemSection | SectionList | ContinuationItem | ClipSection | StructuredDescriptionContent | MacroMarkersList | ProductList | null;
   target_id?: string;
   panel_identifier?: string;
   identifier?: {
@@ -26,7 +27,7 @@ export default class EngagementPanelSectionList extends YTNode {
   constructor(data: RawNode) {
     super();
     this.header = Parser.parseItem(data.header, [ EngagementPanelTitleHeader, OverlayPanelHeader ]);
-    this.content = Parser.parseItem(data.content, [ VideoAttributeView, SectionList, ContinuationItem, ClipSection, StructuredDescriptionContent, MacroMarkersList, ProductList ]);
+    this.content = Parser.parseItem(data.content, [ VideoAttributeView, ItemSection, SectionList, ContinuationItem, ClipSection, StructuredDescriptionContent, MacroMarkersList, ProductList ]);
     this.panel_identifier = data.panelIdentifier;
     this.identifier = data.identifier ? {
       surface: data.identifier.surface,
