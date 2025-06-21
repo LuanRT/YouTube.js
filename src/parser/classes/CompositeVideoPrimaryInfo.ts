b69d035edf2d84e4119d64bca9a32b792b1a774d
Ryuu Mitsuki <dhefam31@gmail.com>
2025-06-21 19:55:55 +0700

    refactor(parser-classes): Remove `data` property from instance class

--- src/parser/classes/CompositeVideoPrimaryInfo.ts
+++ src/parser/classes/CompositeVideoPrimaryInfo.ts
@@ -3,10 +3,9 @@ import type { RawNode } from '../types/RawResponse.js';
 
 export default class CompositeVideoPrimaryInfo extends YTNode {
   static type = 'CompositeVideoPrimaryInfo';
-  data: RawNode;
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   constructor(data: RawNode) {
     super();
-    this.data = data;
   }
 }
