7f230bfe07e301e99d7391674667bfebc8df68fc
Luan <luan.lrt4@gmail.com>
2025-06-22 14:44:58 -0300

    chore: shorten import path

--- src/parser/classes/CompositeVideoPrimaryInfo.ts
+++ src/parser/classes/CompositeVideoPrimaryInfo.ts
@@ -1,5 +1,5 @@
 import { YTNode } from '../helpers.js';
-import type { RawNode } from '../types/RawResponse.js';
+import type { RawNode } from '../types/index.js';
 
 export default class CompositeVideoPrimaryInfo extends YTNode {
   static type = 'CompositeVideoPrimaryInfo';
