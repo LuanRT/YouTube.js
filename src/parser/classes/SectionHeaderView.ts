78dc25dacf502c8248db14170bb30c5777e9ee4c
shinray <shinray@users.noreply.github.com>
2025-06-28 11:41:22 -0700

    Apply suggestions from code review
    Co-authored-by: absidue <48293849+absidue@users.noreply.github.com>

--- src/parser/classes/SectionHeaderView.ts
+++ src/parser/classes/SectionHeaderView.ts
@@ -1,5 +1,6 @@
 import { YTNode } from '../helpers.js';
 import type { RawNode } from '../index.js';
+import Text from './misc/Text.js';
 
 export default class SectionHeaderView extends YTNode {
   static type = 'SectionHeaderView';
