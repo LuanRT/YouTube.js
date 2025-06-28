ec28353d91cf55e4093c891823fab91d0d472b63
skuo <skuo004@ucr.edu>
2025-06-28 12:04:47 -0700

    style: direct import instead of from ytnodes namespace

--- src/parser/classes/GridShelfView.ts
+++ src/parser/classes/GridShelfView.ts
@@ -1,19 +1,21 @@
 import type { ObservedArray } from '../helpers.js';
 import { YTNode } from '../helpers.js';
-import { YTNodes, Parser } from '../index.js';
-import type { RawNode } from '../types/RawResponse.js';
+import { Parser, type RawNode } from '../index.js';
+import ButtonView from './ButtonView.js';
+import SectionHeaderView from './SectionHeaderView.js';
+import ShortsLockupView from './ShortsLockupView.js';
 
 export default class GridShelfView extends YTNode {
   static type = 'GridShelfView';
   
-  contents: ObservedArray<YTNodes.ShortsLockupView> | null;
-  header: YTNodes.SectionHeaderView | null;
+  contents: ObservedArray<ShortsLockupView> | null;
+  header: SectionHeaderView | null;
   content_aspect_ratio: string;
   enable_vertical_expansion: boolean;
-  show_more_button: YTNodes.ButtonView | null;
-  show_less_button: YTNodes.ButtonView | null;
+  show_more_button: ButtonView | null;
+  show_less_button: ButtonView | null;
   min_collapsed_item_count: number;
-  logging_directives: {
+  logging_directives?: {
       tracking_params: string,
       visibility: {
         types: string
@@ -22,12 +24,16 @@ export default class GridShelfView extends YTNode {
   
   constructor(data: RawNode) {
     super();
-    this.contents = Parser.parse(data.contents, true, YTNodes.ShortsLockupView);
-    this.header = Parser.parseItem(data.header, YTNodes.SectionHeaderView);
+    this.contents = Parser.parse(data.contents, true, ShortsLockupView);
+    this.header = Parser.parseItem(data.header, SectionHeaderView);
     this.content_aspect_ratio = data.contentAspectRatio;
     this.enable_vertical_expansion = data.enableVerticalExpansion;
-    this.show_more_button = Parser.parseItem(data.showMoreButton, YTNodes.ButtonView);
-    this.show_less_button = Parser.parseItem(data.showLessButton, YTNodes.ButtonView);
+    this.show_more_button = Parser.parseItem(data.showMoreButton, ButtonView);
+    this.show_less_button = Parser.parseItem(data.showLessButton, ButtonView);
     this.min_collapsed_item_count = data.minCollapsedItemCount;
+    
+    if (Reflect.has(data, 'loggingDirectives')) {
+      this.logging_directives = data.loggingDirectives;
+    }
   }
 }
