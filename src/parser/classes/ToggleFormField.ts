fa9a50aaf389a899d05be40f9f50e0ebcd8a0f29
Luan <luan.lrt4@gmail.com>
2025-07-18 10:32:14 -0300

    chore: clean up & lint

--- src/parser/classes/ToggleFormField.ts
+++ src/parser/classes/ToggleFormField.ts
@@ -5,52 +5,20 @@ import NavigationEndpoint from './NavigationEndpoint.js';
 export default class ToggleFormField extends YTNode {
   static type = 'ToggleFormField';
 
-  label: Text;
-  toggled: boolean;
-  toggle_on_action: {
-    click_tracking_params: string,
-    command_metadata: {
-      web_command_metadata: {
-        send_post: boolean,
-        api_url: string
-      }
-    },
-    playlist_edit_endpoint: NavigationEndpoint
-  };
-  toggle_off_action: {
-    click_tracking_params: string,
-    command_metadata: {
-      web_command_metadata: {
-        send_post: boolean,
-        api_url: string
-      }
-    },
-    playlist_edit_endpoint: NavigationEndpoint
-  };
+  public label: Text;
+  public toggled: boolean;
+  public toggle_on_action?: NavigationEndpoint;
+  public toggle_off_action?: NavigationEndpoint;
 
   constructor(data: RawNode) {
     super();
     this.label = new Text(data.label);
     this.toggled = data.toggled;
-    this.toggle_on_action = {
-      click_tracking_params: data.toggleOnAction.clickTrackingParams,
-      command_metadata: {
-        web_command_metadata: {
-          send_post: data.toggleOnAction.commandMetadata.webCommandMetadata.sendPost,
-          api_url: data.toggleOnAction.commandMetadata.webCommandMetadata.apiUrl
-        }
-      },
-      playlist_edit_endpoint: new NavigationEndpoint(data.toggleOnAction.playlistEditEndpoint)
-    };
-    this.toggle_off_action = {
-      click_tracking_params: data.toggleOffAction.clickTrackingParams,
-      command_metadata: {
-        web_command_metadata: {
-          send_post: data.toggleOffAction.commandMetadata.webCommandMetadata.sendPost,
-          api_url: data.toggleOffAction.commandMetadata.webCommandMetadata.apiUrl
-        }
-      },
-      playlist_edit_endpoint: new NavigationEndpoint(data.toggleOffAction.playlistEditEndpoint)
-    };
+
+    if ('toggleOnAction' in data)
+      this.toggle_on_action = new NavigationEndpoint(data.toggleOnAction);
+
+    if ('toggleOffAction' in data)
+      this.toggle_off_action = new NavigationEndpoint(data.toggleOffAction);
   }
 }
