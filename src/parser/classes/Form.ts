ad3eea943b939af7f8b83d57e6d375b68895c65c
Dave Nicolson <david.nicolson@gmail.com>
2025-10-11 16:37:35 +0200

    feat(parser): Add Form.ts parser class

---
+++ src/parser/classes/Form.ts
@@ -0,0 +1,15 @@
+import { YTNode } from '../helpers.js';
+import { Parser, type RawNode } from '../index.js';
+import { type ObservedArray } from '../helpers.js';
+import ToggleFormField from './ToggleFormField.js';
+
+export default class Form extends YTNode {
+  static type = 'Form';
+
+  fields: ObservedArray<ToggleFormField> | null;
+
+  constructor(data: RawNode) {
+    super();
+    this.fields = Parser.parse(data.fields, true, ToggleFormField);
+  }
+}
