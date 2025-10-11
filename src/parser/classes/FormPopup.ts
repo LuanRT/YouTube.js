519010b7e6e64423d657e9770c1bd17636b04a1c
Dave Nicolson <david.nicolson@gmail.com>
2025-10-11 16:37:42 +0200

    feat(parser): Add FormPopup.ts parser class

---
+++ src/parser/classes/FormPopup.ts
@@ -0,0 +1,21 @@
+import { YTNode } from '../helpers.js';
+import { Parser, type RawNode } from '../index.js';
+import { type ObservedArray } from '../helpers.js';
+import Text from './misc/Text.js';
+import Form from './Form.js';
+import Button from './Button.js';
+
+export default class FormPopup extends YTNode {
+  static type = 'FormPopup';
+
+  title: Text;
+  form: Form | null;
+  buttons: ObservedArray<Button> | null;
+
+  constructor(data: RawNode) {
+    super();
+    this.title = new Text(data.title);
+    this.form = Parser.parseItem(data.form, Form);
+    this.buttons = Parser.parse(data.buttons, true, Button);
+  }
+}
