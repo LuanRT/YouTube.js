3d6ed1def90f5e673969cac650c2250fe98d8768
Dave Nicolson <david.nicolson@gmail.com>
2025-10-12 15:27:01 +0200

    feat(parser): Add `Form.ts` parser class (#1056)
    * feat(parser): Add Form.ts parser class
    
    Co-authored-by: absidue <48293849+absidue@users.noreply.github.com>

--- src/parser/nodes.ts
+++ src/parser/nodes.ts
@@ -181,6 +181,7 @@ export { default as FeedFilterChipBar } from './classes/FeedFilterChipBar.js';
 export { default as FeedNudge } from './classes/FeedNudge.js';
 export { default as FeedTabbedHeader } from './classes/FeedTabbedHeader.js';
 export { default as FlexibleActionsView } from './classes/FlexibleActionsView.js';
+export { default as Form } from './classes/Form.js';
 export { default as FormFooterView } from './classes/FormFooterView.js';
 export { default as GameCard } from './classes/GameCard.js';
 export { default as GameDetails } from './classes/GameDetails.js';
