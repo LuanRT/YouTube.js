6b0ca498dad3fae4b336e3587482f559008bab1d
absidue <48293849+absidue@users.noreply.github.com>
2026-02-08 23:32:53 +0100

    feat(Search)!: Update search filters to match YouTube changes

--- src/types/Misc.ts
+++ src/types/Misc.ts
@@ -4,17 +4,17 @@ export type InnerTubeConfig = SessionOptions;
 export type InnerTubeClient = 'IOS' | 'WEB' | 'MWEB' | 'ANDROID' | 'YTMUSIC' | 'YTMUSIC_ANDROID' | 'YTSTUDIO_ANDROID' | 'TV' | 'TV_SIMPLY' |'TV_EMBEDDED' | 'YTKIDS' | 'WEB_EMBEDDED' | 'WEB_CREATOR';
 export type EngagementType = 'ENGAGEMENT_TYPE_UNBOUND' | 'ENGAGEMENT_TYPE_VIDEO_LIKE' | 'ENGAGEMENT_TYPE_VIDEO_DISLIKE' | 'ENGAGEMENT_TYPE_SUBSCRIBE' | 'ENGAGEMENT_TYPE_PLAYBACK' | 'ENGAGEMENT_TYPE_YPC_GET_PREMIUM_PAGE' | 'ENGAGEMENT_TYPE_YPC_GET_DOWNLOAD_ACTION';
 
-export type UploadDate = 'all' | 'hour' | 'today' | 'week' | 'month' | 'year';
-export type SearchType = 'all' | 'video' | 'channel' | 'playlist' | 'movie';
-export type Duration = 'all' | 'short' | 'medium' | 'long';
-export type SortBy = 'relevance' | 'rating' | 'upload_date' | 'view_count';
+export type UploadDate = 'all' | 'today' | 'week' | 'month' | 'year';
+export type SearchType = 'all' | 'video' | 'shorts' | 'channel' | 'playlist' | 'movie';
+export type Duration = 'all' | 'over_twenty_mins' | 'under_three_mins' | 'three_to_twenty_mins';
+export type Prioritize = 'relevance' | 'popularity';
 export type Feature = 'hd' | 'subtitles' | 'creative_commons' | '3d' | 'live' | 'purchased' | '4k' | '360' | 'location' | 'hdr' | 'vr180';
 
 export type SearchFilters = {
   upload_date?: UploadDate;
   type?: SearchType;
   duration?: Duration;
-  sort_by?: SortBy;
+  prioritize?: Prioritize;
   features?: Feature[];
 };
 
