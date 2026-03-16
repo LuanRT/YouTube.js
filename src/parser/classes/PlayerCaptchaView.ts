e7a3b0cc47d217dd90c291d1662b3c42b6828d3f
LuanRT <luan.lrt4@gmail.com>
2026-03-16 11:21:01 -0300

    fix(parser): Resolve some old parser issues
    Should reduce parser warning spam quite a bit.
    
    Closes #653
    Closes #781
    Closes #1121 (minus unrelated pot issue)
    Closes #978
    Closes #1053
    Closes #695
    Closes #1098

---
+++ src/parser/classes/PlayerCaptchaView.ts
@@ -0,0 +1,37 @@
+import { YTNode } from '../helpers.js';
+import { Text } from '../misc.js';
+import { type RawNode } from '../index.js';
+
+export default class PlayerCaptchaView extends YTNode {
+  static type = 'PlayerCaptchaView';
+
+  public captcha_loading_message?: Text;
+  public challenge_reason?: Text;
+  public captcha_successful_message?: Text;
+  public captcha_cookie_set_failure_message?: Text;
+  public captcha_failed_message?: Text;
+
+  constructor(data: RawNode) {
+    super();
+
+    if ('captchaLoadingMessage' in data) {
+      this.captcha_loading_message = Text.fromAttributed(data.captchaLoadingMessage);
+    }
+
+    if ('challengeReason' in data) {
+      this.challenge_reason = Text.fromAttributed(data.challengeReason);
+    }
+
+    if ('captchaSuccessfulMessage' in data) {
+      this.captcha_successful_message = Text.fromAttributed(data.captchaSuccessfulMessage);
+    }
+
+    if ('captchaCookieSetFailureMessage' in data) {
+      this.captcha_cookie_set_failure_message = Text.fromAttributed(data.captchaCookieSetFailureMessage);
+    }
+
+    if ('captchaFailedMessage' in data) {
+      this.captcha_failed_message = Text.fromAttributed(data.captchaFailedMessage);
+    }
+  }
+}
