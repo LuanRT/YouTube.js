39158838f1b4a8c414ddbabd94b5a48198b16b2f
LuanRT <luan.lrt4@gmail.com>
2026-03-19 11:23:05 -0300

    feat(ChipBarView): Parse `renderer_context`

--- src/parser/classes/ChipBarView.ts
+++ src/parser/classes/ChipBarView.ts
@@ -1,14 +1,22 @@
 import { YTNode, type ObservedArray } from '../helpers.js';
 import { Parser, type RawNode } from '../index.js';
+import { RendererContext } from '../misc.js';
 import ChipView from './ChipView.js';
 
 export default class ChipBarView extends YTNode {
   static type = 'ChipBarView';
 
-  chips: ObservedArray<ChipView>;
+  public chips: ObservedArray<ChipView>;
+  public chip_bar_state_entity_key?: string;
+  public renderer_context?: RendererContext;
 
   constructor(data: RawNode) {
     super();
     this.chips = Parser.parseArray(data.chips, ChipView);
+    this.chip_bar_state_entity_key = data.chipBarStateEntityKey;
+
+    if ('rendererContext' in data) {
+      this.renderer_context = new RendererContext(data.rendererContext);
+    }
   }
 }
