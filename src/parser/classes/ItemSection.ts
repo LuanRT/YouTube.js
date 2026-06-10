96516996376af0d2fdbabb946977fedad7b33003
Dave Nicolson <david.nicolson@gmail.com>
2026-06-10 18:58:52 +0200

    fix(ItemSection): ChipBarView parser warning

--- src/parser/classes/ItemSection.ts
+++ src/parser/classes/ItemSection.ts
@@ -5,18 +5,19 @@ import ItemSectionTabbedHeader from './ItemSectionTabbedHeader.js';
 import CommentsHeader from './comments/CommentsHeader.js';
 import SortFilterHeader from './SortFilterHeader.js';
 import FeedFilterChipBar from './FeedFilterChipBar.js';
+import ChipBarView from './ChipBarView.js';
 
 export default class ItemSection extends YTNode {
   static type = 'ItemSection';
 
-  header: CommentsHeader | ItemSectionHeader | ItemSectionTabbedHeader | SortFilterHeader | FeedFilterChipBar | null;
+  header: CommentsHeader | ItemSectionHeader | ItemSectionTabbedHeader | SortFilterHeader | FeedFilterChipBar | ChipBarView | null;
   contents: ObservedArray<YTNode>;
   target_id?: string;
   continuation?: string;
 
   constructor(data: RawNode) {
     super();
-    this.header = Parser.parseItem(data.header, [ CommentsHeader, ItemSectionHeader, ItemSectionTabbedHeader, SortFilterHeader, FeedFilterChipBar ]);
+    this.header = Parser.parseItem(data.header, [ CommentsHeader, ItemSectionHeader, ItemSectionTabbedHeader, SortFilterHeader, FeedFilterChipBar, ChipBarView ]);
     this.contents = Parser.parseArray(data.contents);
 
     if (data.targetId || data.sectionIdentifier) {
