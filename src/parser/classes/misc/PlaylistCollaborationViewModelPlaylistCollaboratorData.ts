2b4f1bb6890e1fbe78dae98d645bc371574781d4
LuanRT <luan.lrt4@gmail.com>
2026-08-12 19:04:03 -0300

    fix(PlaylistCollaborationViewModelPlaylistCollaboratorData): Change to snake case (oops)

--- src/parser/classes/misc/PlaylistCollaborationViewModelPlaylistCollaboratorData.ts
+++ src/parser/classes/misc/PlaylistCollaborationViewModelPlaylistCollaboratorData.ts
@@ -2,13 +2,13 @@ import type { YTNode } from '../../helpers.js';
 import { Parser, type RawNode } from '../../index.js';
 
 export default class PlaylistCollaborationViewModelPlaylistCollaboratorData {
-  public removeCollaboratorConfirmationDialog: YTNode | null;
-  public externalChannelId?: string;
-  public collaboratorContentListItem: YTNode | null;
+  public remove_collaborator_confirmation_dialog: YTNode | null;
+  public external_channel_id?: string;
+  public collaborator_content_list_item: YTNode | null;
 
   constructor(data: RawNode) {
-    this.removeCollaboratorConfirmationDialog = Parser.parseItem(data.removeCollaboratorConfirmationDialog);
-    this.externalChannelId = data.externalChannelId;
-    this.collaboratorContentListItem = Parser.parseItem(data.collaboratorContentListItem);
+    this.remove_collaborator_confirmation_dialog = Parser.parseItem(data.removeCollaboratorConfirmationDialog);
+    this.external_channel_id = data.externalChannelId;
+    this.collaborator_content_list_item = Parser.parseItem(data.collaboratorContentListItem);
   }
 }
