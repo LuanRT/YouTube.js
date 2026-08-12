e082ee5c0b8db4f5946eb94fce374cca1fab83bf
LuanRT <luan.lrt4@gmail.com>
2026-08-12 20:54:59 -0300

    chore: lint

--- src/parser/classes/AvatarStackView.ts
+++ src/parser/classes/AvatarStackView.ts
@@ -5,6 +5,7 @@ import { Parser } from '../index.js';
 import Text from './misc/Text.js';
 import AvatarView from './AvatarView.js';
 import RendererContext from './misc/RendererContext.js';
+
 export default class AvatarStackView extends YTNode {
   static type = 'AvatarStackView';
 
