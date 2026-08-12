745db99f5ec621368cc5150d0c69aaecc7d211a6
Illusion137 <raygoza.d@hotmail.com>
2026-08-12 03:52:11 -0700

    chore(YouTube Studio Web): I forgot to commit the updated types from last commit

--- src/types/StudioWebUploading.ts
+++ src/types/StudioWebUploading.ts
@@ -70,3 +70,214 @@ export interface UploadVideoDetails {
         overwrite?: boolean;
     };
 };
+
+export interface CreatorVideo {
+  videoId: string;
+  channelId?: string;
+  title?: string;
+  description?: string;
+  privacy?: 'VIDEO_PRIVACY_PUBLIC' | 'VIDEO_PRIVACY_UNLISTED' | 'VIDEO_PRIVACY_PRIVATE';
+  status?: 'VIDEO_STATUS_PROCESSED';
+  draftStatus?: 'DRAFT_STATUS_NONE';
+  shareUrl?: string;
+  watchUrl?: string;
+  lengthSeconds?: string;
+  videoDurationMs?: number;
+  timeCreatedSeconds?: string;
+  timePublishedSeconds?: string;
+  thumbnailDetails?: { thumbnails: { url: string, width: number, height: number }[] };
+};
+
+// [Raw Types]
+
+export interface CreateVideoResponse {
+  responseContext: ResponseContext;
+  videoId: string;
+  contents: Contents;
+}
+
+export interface ResponseContext {
+  serviceTrackingParams: ServiceTrackingParam[];
+  consistencyTokenJar: ConsistencyTokenJar;
+  stateTags: StateTags;
+  responseId: string;
+  webResponseContextExtensionData: WebResponseContextExtensionData;
+}
+
+export interface ServiceTrackingParam {
+  service: string;
+  params: Param[];
+}
+
+export interface Param {
+  key: string;
+  value: string;
+}
+
+export interface ConsistencyTokenJar {
+  encryptedTokenJarContents: string;
+  expirationSeconds: string;
+}
+
+export interface StateTags {
+  stateTagsModified: number[];
+}
+
+export interface WebResponseContextExtensionData {
+  hasDecorated: boolean;
+  challenge?: { type?: 'CHALLENGE_PROMPT_TYPE_AUTHENTICATE' }
+}
+
+export interface Contents {
+  uploadFeedbackItemRenderer: UploadFeedbackItemRenderer;
+}
+
+export interface UploadFeedbackItemRenderer {
+  id: Id;
+  continuations: Continuation[];
+  dataFreshnessEntity: DataFreshnessEntity;
+}
+
+export interface Id {
+  frontendUploadId: string;
+  videoId: string;
+}
+
+export interface Continuation {
+  timedContinuationData?: TimedContinuationData;
+  uploadFeedbackRefreshContinuation?: UploadFeedbackRefreshContinuation;
+}
+
+export interface TimedContinuationData {
+  timeoutMs: number;
+  continuation: string;
+  clickTrackingParams: string;
+}
+
+export interface UploadFeedbackRefreshContinuation {
+  continuation: string;
+  continueInMs: number;
+  clickTrackingParams: string;
+}
+
+export interface DataFreshnessEntity {
+  key: string;
+  lastUpdated: LastUpdated;
+}
+
+export interface LastUpdated {
+  seconds: string;
+  nanos: number;
+}
+
+// [FeedbackContinuation] ==========================================================
+
+export type ContinuationsUploadFeedback = ContinuationUploadFeedback[];
+
+export interface ContinuationUploadFeedback {
+  uploadFeedbackItemContinuation: UploadFeedbackItemContinuation;
+}
+
+export interface UploadFeedbackItemContinuation {
+  id: Id;
+  contents: Content[];
+  continuations: Continuation[];
+  dataFreshnessEntity: DataFreshnessEntity;
+}
+
+export interface Id {
+  frontendUploadId: string;
+  videoId: string;
+}
+
+export interface Content {
+  transferProgressBar?: TransferProgressBar;
+  uploadChecksRenderer?: UploadChecksRenderer;
+}
+
+export interface TransferProgressBar {
+  fractionCompleted: number;
+  progressMessage: ProgressMessage;
+}
+
+export interface ProgressMessage {
+  simpleText: string;
+}
+
+export interface UploadChecksRenderer {
+  checksDataVideoMonetized: ChecksDataVideoMonetized;
+  checksDataVideoNotMonetized: ChecksDataVideoNotMonetized;
+}
+
+export interface ChecksDataVideoMonetized {
+  checksSummary: ChecksSummary;
+  copyrightCheck: CopyrightCheck;
+  adSuitabilityCheck: AdSuitabilityCheck;
+  communityGuidelinesCheck: CommunityGuidelinesCheck;
+}
+
+export interface ChecksSummary {
+  status: string;
+}
+
+export interface CopyrightCheck {
+  checkStatus: string;
+}
+
+export interface AdSuitabilityCheck {
+  checkStatus: string;
+}
+
+export interface CommunityGuidelinesCheck {
+  checkStatus: string;
+}
+
+export interface ChecksDataVideoNotMonetized {
+  checksSummary: ChecksSummary2;
+  copyrightCheck: CopyrightCheck2;
+  adSuitabilityCheck: AdSuitabilityCheck2;
+  communityGuidelinesCheck: CommunityGuidelinesCheck2;
+}
+
+export interface ChecksSummary2 {
+  status: string;
+}
+
+export interface CopyrightCheck2 {
+  checkStatus: string;
+}
+
+export interface AdSuitabilityCheck2 {
+  checkStatus: string;
+}
+
+export interface CommunityGuidelinesCheck2 {
+  checkStatus: string;
+}
+
+export interface Continuation {
+  timedContinuationData?: TimedContinuationData;
+  uploadFeedbackRefreshContinuation?: UploadFeedbackRefreshContinuation;
+}
+
+export interface TimedContinuationData {
+  timeoutMs: number;
+  continuation: string;
+  clickTrackingParams: string;
+}
+
+export interface UploadFeedbackRefreshContinuation {
+  continuation: string;
+  continueInMs: number;
+  clickTrackingParams: string;
+}
+
+export interface DataFreshnessEntity {
+  key: string;
+  lastUpdated: LastUpdated;
+}
+
+export interface LastUpdated {
+  seconds: string;
+  nanos: number;
+}
