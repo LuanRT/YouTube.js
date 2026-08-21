96afef5749c87ab9d5c74f56d50fdd709e2c2b70
Illusion137 <raygoza.d@hotmail.com>
2026-08-21 04:51:06 -0700

    fix(BotGuard): make challenge info interface all required data

--- src/types/BotGuard.ts
+++ src/types/BotGuard.ts
@@ -2,11 +2,11 @@ import type { IBotguardChallenge, IGetChallengeResponse } from '../parser/index.
 
 export interface BotGuardSolverChallenge extends IBotguardChallenge {
   ytcfg?: Record<string, unknown>;
-};
+}
 
-export interface BotGuardChallengeInfo extends IGetChallengeResponse {
-  bg_challenge?: BotGuardSolverChallenge;
-} 
+export interface BotGuardChallengeInfo extends Required<IGetChallengeResponse> {
+  bg_challenge: BotGuardSolverChallenge;
+}
 
 export interface BotGuardSolver<T> {
   solve: (botguard_challenge: BotGuardSolverChallenge, binding: T) => Promise<string>;
