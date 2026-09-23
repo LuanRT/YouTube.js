d690e2018176c9e3866ac7d2352b15e5c554a2c6
feather <149301458+featheries@users.noreply.github.com>
2026-09-24 04:03:43 +0700

    Apply batched suggestions from code review
    Co-authored-by: Luan <luan.lrt4@gmail.com>

--- src/parser/classes/InterstitialView.ts
+++ src/parser/classes/InterstitialView.ts
@@ -6,34 +6,18 @@ import ButtonView from './ButtonView.js';
 export default class InterstitialView extends YTNode {
   static type = 'InterstitialView';
 
-  title: Text;
-  description: Text;
-  icon?: Thumbnail[];
-  primary_button?: ButtonView | null;
-  logging_directives?: {
-    tracking_params: string,
-    visibility: {
-      types: string
-    }
-  };
+  public title: Text;
+  public description: Text;
+  public icon?: Thumbnail[];
+  public primary_button: ButtonView | null;
 
   constructor(data: RawNode) {
     super();
     this.title = Text.fromAttributed(data.title);
     this.description = Text.fromAttributed(data.description);
-    if (Reflect.has(data, 'icon')) {
+    if ('icon' in data) {
       this.icon = Thumbnail.fromResponse(data.icon);
     }
-    if (Reflect.has(data, 'primaryButton')) {
-      this.primary_button = Parser.parseItem(data.primaryButton, ButtonView);
-    }
-    if (Reflect.has(data, 'loggingDirectives')) {
-      this.logging_directives = {
-        tracking_params: data.loggingDirectives.trackingParams,
-        visibility: {
-          types: data.loggingDirectives.visibility.types
-        }
-      };
-    }
+    this.primary_button = Parser.parseItem(data.primaryButton, ButtonView);
   }
 }
--- src/parser/classes/PlayerInterstitial.ts
+++ src/parser/classes/PlayerInterstitial.ts
@@ -5,13 +5,13 @@ import InterstitialView from './InterstitialView.js';
 export default class PlayerInterstitial extends YTNode {
   static type = 'PlayerInterstitial';
 
-  content: InterstitialView | null;
-  state_entity_store_key?: string;
+  public content: InterstitialView | null;
+  public state_entity_store_key?: string;
 
   constructor(data: RawNode) {
     super();
     this.content = Parser.parseItem(data.content, InterstitialView);
-    if (Reflect.has(data, 'stateEntityStoreKey')) {
+    if ('stateEntityStoreKey' in data) {
       this.state_entity_store_key = data.stateEntityStoreKey;
     }
   }
