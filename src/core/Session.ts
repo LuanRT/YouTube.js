63a12fe3ba8f8c9ee4a1b957d00b57804d2751c5
Illusion137 <raygoza.d@hotmail.com>
2026-09-23 19:09:22 -0700

    feat(Session): add `PartialContext`

--- src/core/Session.ts
+++ src/core/Session.ts
@@ -123,6 +123,13 @@ export type Context = {
   };
 }
 
+export type PartialContext = {
+  client?: Partial<Context['client']>;
+  user?: Partial<Context['user']>;
+  thirdParty?: Partial<Context['thirdParty']>;
+  request?: Partial<Context['request']>;
+};
+
 type ContextData = {
   hl: string;
   gl: string;
